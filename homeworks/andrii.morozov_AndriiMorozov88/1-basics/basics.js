4a0370c93206918656dd554255b6b98da39f2147
Andrii Morozov <slon88888@ukr.net>
2022-11-16 23:44:20 +0200

    fixed linter mistakes

--- homeworks/andrii.morozov_AndriiMorozov88/1-basics/basics.js
+++ homeworks/andrii.morozov_AndriiMorozov88/1-basics/basics.js
@@ -1,12 +1,12 @@
 // Pythagorean theorem
-let legAC = 'a';
-let legBC = 'b';
-let hypotenuseAB = `${legAC}2 + ${legBC}2`
+const legAC = 'a';
+const legBC = 'b';
+const hypotenuseAB = `${legAC}2 + ${legBC}2`;
 console.log(`c2 = ${hypotenuseAB}`); // c2 = a2 + b2
-// How much apples i can buy if i have 50 usd, apples cost 3 usd. and how much money i will have after
+// How much apples i can buy if i have 50 usd, apples cost 3 usd. how much money i will have after
 const ownMoney = 50;
 const priceForKg = 3;
-let moneyLeft = ownMoney%priceForKg;
-let appleAmount = (ownMoney - moneyLeft) / priceForKg;
+const moneyLeft = ownMoney%priceForKg;
+const appleAmount = (ownMoney - moneyLeft) / priceForKg;
 console.log(`Max amount of apple ${appleAmount} kg`); // Max amount of apple 16 kg
 console.log(`left ${moneyLeft} usd`); // left 2 usd
