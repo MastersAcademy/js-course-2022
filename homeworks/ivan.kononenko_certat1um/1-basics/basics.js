3aaa50f8c4e7854531ccd466e92cb6d0ca8165b8
certat1um <canvasworkst@gmail.com>
2022-11-17 00:33:32 +0200

    finished 1-basics

---
+++ homeworks/ivan.kononenko_certat1um/1-basics/basics.js
@@ -0,0 +1,22 @@
+const first_num = 5;
+const second_num = 12;
+
+let result;
+
+result = first_num + second_num;
+console.log(result);
+
+result = first_num - second_num;
+console.log(result);
+
+result = first_num * second_num;
+console.log(result);
+
+result = first_num / second_num;
+console.log(result);
+
+result = first_num % second_num;
+console.log(result);
+
+result = first_num ** second_num;
+console.log(result);
