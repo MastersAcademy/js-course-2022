d5e2a012cae0957cf056e8781368c4a88df129f2
Влад Дихтярук <vladdihtaruk@MacBook-Pro-Vlad.local>
2022-11-17 10:42:52 +0100

    add first homework

--- homeworks/vladyslav.dikhtiaruk_vladdikhtia/1-basics/basics.js
+++ homeworks/vladyslav.dikhtiaruk_vladdikhtia/1-basics/basics.js
@@ -1,26 +1,26 @@
-let firstNumber = 10;
-let secondNumber = 5;
-let thirdNumber = 27;
+const firstNumber = 10;
+const secondNumber = 5;
+const thirdNumber = 27;
 const fourthNumber = 87;
 
-console.log(firstNumber+secondNumber);
-console.log(firstNumber+secondNumber-fourthNumber+thirdNumber);
+console.log(firstNumber + secondNumber);
+console.log(firstNumber + secondNumber - fourthNumber + thirdNumber);
 console.log(secondNumber - thirdNumber);
 console.log(fourthNumber * firstNumber);
-console.log(thirdNumber * (firstNumber+secondNumber));
+console.log(thirdNumber * (firstNumber + secondNumber));
 console.log(fourthNumber / firstNumber);
 console.log(firstNumber % secondNumber);
 console.log(fourthNumber % (fourthNumber - thirdNumber - (secondNumber ** 2)));
-console.log((firstNumber ** secondNumber) - (secondNumber ** firstNumber )) ;
+console.log((firstNumber ** secondNumber) - (secondNumber ** firstNumber));
 
 
-let firstStringNumber = '123';
+const firstStringNumber = '123';
 const secondStringNumber = '43';
-const fNumber = Number(firstStringNumber); // fNumber = 123;
-let sNUmber = Number(secondStringNumber); // sNumber= 43;
-console.log(fNumber, typeof fNumber, fNumber + sNUmber);
+const fNumber = Number(firstStringNumber); //fNumber = 123;
+const sNumber = Number(secondStringNumber); //sNumber= 43;
+console.log(fNumber, typeof fNumber, fNumber + sNumber);
 
-let a = true;
+const a = true;
 const b = Number(a); // b = 1
 console.log(b - a);
 console.log(a + b);
