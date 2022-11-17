f30599673cd0ef59d0cca8a509e2c4708958b493
Andrey-Shtanko <97941755+Andrey-Shtanko@users.noreply.github.com>
2022-11-17 12:19:40 +0200

    first-HW-done

---
+++ homeworks/andriy.shtanko_Andrey-Shtanko/1-basics/basics.js
@@ -0,0 +1,23 @@
+const numberA = 10;
+const numberB = 2;
+const numberC = 5;
+
+let messageFirstPart;
+let messageSecondPart;
+
+console.log(numberA + numberB);
+
+messageFirstPart = "First";
+messageSecondPart = "HomeWork";
+
+console.log(messageFirstPart + messageSecondPart);
+console.log(`${messageFirstPart} ${messageSecondPart}`);
+
+console.log(numberA + numberB - numberC);
+console.log(numberA - numberC);
+console.log(numberA * numberB);
+console.log(numberA / numberC);
+console.log(numberC % numberB);
+console.log(numberA % numberC);
+console.log(numberB ** numberC);
+console.log(numberB ** numberA);
