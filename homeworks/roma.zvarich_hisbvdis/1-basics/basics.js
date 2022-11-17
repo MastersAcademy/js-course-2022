cef199e6b44d4e0f0857d21798075ddc14a274e6
hisbvdis <hisbvdis@gmail.com>
2022-11-17 19:23:26 +0200

    Homework 1

---
+++ homeworks/roma.zvarich_hisbvdis/1-basics/basics.js
@@ -0,0 +1,9 @@
+const a = 1;
+const b = 2;
+
+const sum = a + b;
+const subtraction = a - b;
+const multiplication = a * b;
+const division = a / b;
+const remainder = b % 2;
+const exponentiation = a ** b;
