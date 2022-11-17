93d68a87730b75b7e9e6fed1e493ceb3f7e49c8d
Andrey-Shtanko <97941755+Andrey-Shtanko@users.noreply.github.com>
2022-11-17 12:29:18 +0200

    Update basics.js

--- homeworks/andriy.shtanko_Andrey-Shtanko/1-basics/basics.js
+++ homeworks/andriy.shtanko_Andrey-Shtanko/1-basics/basics.js
@@ -4,18 +4,20 @@ const numberC = 5;
 
 console.log(numberA + numberB);
 
-const messageFirstPart = "First";
-const messageSecondPart = "HomeWork";
-console.log(messageFirstPart + messageSecondPart);
-console.log(`${messageFirstPart} ${messageSecondPart}`);
-
 console.log(numberA + numberB - numberC);
+
 console.log(numberA - numberC);
+
 console.log(numberA * numberB);
+
 console.log(numberA / numberC);
+
 console.log(numberC % numberB);
+
 console.log(numberA % numberC);
+
 console.log(numberB ** numberC);
+
 console.log(numberB ** numberA);
 
 /*
