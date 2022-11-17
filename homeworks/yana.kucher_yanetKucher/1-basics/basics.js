0b3506497d5b27cde2a6b49cd41ffbaaf6154782
yana-kucher <yana.panasenko@gmail.com>
2022-11-17 21:15:55 +0200

    performed arithmetic operations

---
+++ homeworks/yana.kucher_yanetKucher/1-basics/basics.js
@@ -0,0 +1,15 @@
+const firstNumber = 90;
+const secondNumber = 40;
+const subtractionResult = (firstNumber - secondNumber);
+const additionResult = (firstNumber + secondNumber);
+const multiplicationResult = (firstNumber * secondNumber);
+const divisionResult = (firstNumber / secondNumber);
+const remainderDivisionResult = (firstNumber % secondNumber);
+const exponentiationResult = (firstNumber ** secondNumber);
+
+console.log(`subtraction result: ${subtractionResult}`);
+console.log(`addition result: ${additionResult}`);
+console.log(`multiplication result: ${multiplicationResult}`);
+console.log(`division result: ${divisionResult}`);
+console.log(`remainder of division result: ${remainderDivisionResult}`);
+console.log(`exponentiation result: ${exponentiationResult}`);
