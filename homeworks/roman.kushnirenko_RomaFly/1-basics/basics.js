16791f91de08599c60a323a7fbe3db5467c7cdbb
RomaFly <romfly7@gmail.com>
2022-11-17 10:16:05 +0200

    Added js with task

---
+++ homeworks/roman.kushnirenko_RomaFly/1-basics/basics.js
@@ -0,0 +1,11 @@
+const numFirst = 15;
+const numSecond = 7;
+
+console.log('*** Арефметичні операції над двома числами: 15 і 7 ***');
+
+console.log(`Додавання: ${numFirst + numSecond}`);
+console.log(`Віднімання: ${numFirst - numSecond}`);
+console.log(`Множення: ${numFirst * numSecond}`);
+console.log(`Ділення: ${(numFirst / numSecond).toFixed(2)}`);
+console.log(`Остача від ділення: ${numFirst % numSecond}`);
+console.log(`Піднесення до степеня: ${numFirst ** numSecond}`);
