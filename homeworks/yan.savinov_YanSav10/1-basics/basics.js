f3003d2c7c291c5100aa32de51339d63f5a52f69
Yan Savinov <yansavinov@gmail.com>
2022-11-17 14:20:42 +0200

    Added basics.js

--- homeworks/yan.savinov_YanSav10/1-basics/basics.js
+++ homeworks/yan.savinov_YanSav10/1-basics/basics.js
@@ -2,19 +2,19 @@ const firstNumber = 10;
 const secondNumber = 2;
 
 const additionResult = firstNumber + secondNumber;
-console.log('Результат додавання ' + additionResult);
+console.log(`Результат додавання ${additionResult}`);
 
 const subtractionResult = firstNumber - secondNumber;
-console.log('Результат віднімання ' + subtractionResult);
+console.log(`Результат віднімання ${subtractionResult}`);
 
 const multiplicationResult = firstNumber * secondNumber;
-console.log('Результат множення ' + multiplicationResult);
+console.log(`Результат множення  ${multiplicationResult}`);
 
 const divisionResult = firstNumber / secondNumber;
-console.log('Результат ділення ' + divisionResult);
+console.log(`Результат ділення  ${divisionResult}`);
 
 const remainderResult = firstNumber % secondNumber;
-console.log('Результат остачі від ділення ' + remainderResult);
+console.log(`Результат остачі від ділення ${remainderResult}`);
 
 const exponentiationResult = firstNumber ** secondNumber;
-console.log('Результат піднесення до степеня ' + exponentiationResult);
+console.log(`Результат піднесення до степеня ${exponentiationResult}`);
