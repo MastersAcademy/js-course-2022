a64030264a7a4236caf9fdf85c437a30a3457c49
Igor Zapisochnyy <109870514+IgorFromUA@users.noreply.github.com>
2022-11-17 12:22:58 +0200

    removed addition String

--- homeworks/ihor.zapisochnyy_IgorFromUA/1-basics/basics.js
+++ homeworks/ihor.zapisochnyy_IgorFromUA/1-basics/basics.js
@@ -3,14 +3,9 @@
 // оголошення змінних
 const firstNumber = 20;
 const secondNumber = 5;
-const firstString = 'Hello';
-const secondString = 'world!';
 let result;
 
 // додавання
-result = firstString + ' ' + secondString;
-console.log(result);
-
 result = firstNumber + secondNumber;
 console.log(firstNumber + ' додати ' + secondNumber + ' дорівнює ' + result);
 
