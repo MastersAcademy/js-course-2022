7232aaa0f725e1c518fb4ca07229e5ae5a273907
Vladislav Buzunar <98417449+buzyaka@users.noreply.github.com>
2022-11-18 03:40:03 +0200

    Add files via upload

--- homeworks/vladislav.buzunar_buzyaka/1-basics/basics.js
+++ homeworks/vladislav.buzunar_buzyaka/1-basics/basics.js
@@ -1,41 +1,41 @@
-// Variablenumbers 
+// Variable numbers 
 
 const firstNumber = 7;
 const secondNumber = 2;
 const thirdNumber = 5;
-// const myName = `Vlad`; 
-// const firstSomeText = `I love Masters Academy`; 
-// const secondSomeText = `I love JS`; 
+// const myName = `Vlad` ;
+// const firstSomeText = `I love Masters Academy` ;
+// const secondSomeText = `I love JS` ;
 
-// Actions numbers 
+// Actions numbers
 
 const sumAllNumbers = firstNumber + secondNumber + thirdNumber;
 const multAllNumbers = firstNumber * secondNumber * thirdNumber;
 const divisAllNumbers = firstNumber / secondNumber / thirdNumber;
 const remainOfNumbers = firstNumber % secondNumber;
 
-// (firstNumber % thirdNumber); 
-// (secondNumber % firstNumber); 
-// (secondNumber % thirdNumber); 
-// (thirdNumber % firstNumber); 
-// (thirdNumber % secondNumber); 
+// (firstNumber % thirdNumber);
+// (secondNumber % firstNumber);
+// (secondNumber % thirdNumber);
+// (thirdNumber % firstNumber);
+// (thirdNumber % secondNumber);
 
-// Output to the Console 
+// Output to the Console
 
 console.log(sumAllNumbers);
 console.log(multAllNumbers);
 console.log(divisAllNumbers);
 console.log(remainOfNumbers);
 
-// elevation to the square 
+// elevation to the square
 console.log(`${firstNumber} squared is equal to ${firstNumber ** 2}`);
 console.log(`${firstNumber} squared is equal to ${secondNumber ** 2}`);
 console.log(`${firstNumber} squared is equal to ${thirdNumber ** 2}`);
 
-// concatenation (не брал git, писал что неожиданная конкатенация) 
+// concatenation (не брал git, писал что неожиданная конкатенация)
 
-// console.log(`Hey, my name is ${myName}, i am 17y.o . ` + firstSomeText + `. ` + secondSomeText); 
- /************************************************ 
+// console.log(`Hey, my name is ${myName}, i am 17y.o . ` + firstSomeText + `. ` + secondSomeText);
+ /************************************************
 console.log(`First number is ` + firstNumber + `. And second number is ` + secondNumber +
 `. And third number is ` + thirdNumber);
-*************************************************/ 
+*************************************************/
