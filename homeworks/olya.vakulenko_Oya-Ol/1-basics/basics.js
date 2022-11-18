e1e3b18f593272bd809f02222300dc8b479b948a
Olga Vakulenko <vakulenkooya@gmail.com>
2022-11-18 21:39:59 +0200

    add new expressions

--- homeworks/olya.vakulenko_Oya-Ol/1-basics/basics.js
+++ homeworks/olya.vakulenko_Oya-Ol/1-basics/basics.js
@@ -11,3 +11,19 @@ console.log('Result:' + firstNumber * thirdNumber);
 console.log('Result:' + thirdNumber / secondNumber);
 console.log('Result:' + (secondNumber % thirdNumber));
 console.log('Result:' + firstNumber ** thirdNumber);
+
+let value = 10;
+
+value += 5;
+value -= 5;
+value *= 10;
+value /= 2;
+value %= 3;
+
+console.log(value);
+
+const newYear = 2023;
+const userName = 'Valera';
+const greeting = `Welcome ${userName} to ${newYear}!`;
+
+console.log(greeting);
