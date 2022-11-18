59ea3973b55b7a9b06303a380a7cb14b591a016b
Olga Vakulenko <vakulenkooya@gmail.com>
2022-11-18 18:58:53 +0200

    add homework basics

--- homeworks/olya.vakulenko_Oya-Ol/1-basics/basics.js
+++ homeworks/olya.vakulenko_Oya-Ol/1-basics/basics.js
@@ -4,12 +4,10 @@ let firstNumber = 2;
 let secondNumber = 5;
 let thirdNumber = 10;
 
-console.log("Result:" + (firstNumber + secondNumber));
-console.log("Result:" + (thirdNumber - secondNumber));
-console.log("Result:" + (thirdNumber - firstNumber));
-console.log("Result:" + firstNumber * thirdNumber);
-console.log("Result:" + thirdNumber / secondNumber);
-console.log("Result:" + (secondNumber % thirdNumber));
-console.log("Result:" + firstNumber ** thirdNumber);
-
-// console.log("Hello, World!");
+console.log('Result:' + (firstNumber + secondNumber));
+console.log('Result:' + (thirdNumber - secondNumber));
+console.log('Result:' + (thirdNumber - firstNumber));
+console.log('Result:' + firstNumber * thirdNumber);
+console.log('Result:' + thirdNumber / secondNumber);
+console.log('Result:' + (secondNumber % thirdNumber));
+console.log('Result:' + firstNumber ** thirdNumber);
