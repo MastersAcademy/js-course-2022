1cd87acbd21aa2d8da5140240e674c37f2e8bb91
hisbvdis <hisbvdis@gmail.com>
2022-11-18 12:27:36 +0200

    Single quotes

--- homeworks/roma.zvarich_hisbvdis/1-basics/basics.js
+++ homeworks/roma.zvarich_hisbvdis/1-basics/basics.js
@@ -8,9 +8,9 @@ const division = a / b;
 const remainder = b % 2;
 const exponentiation = a ** b;
 
-console.log("Sum value is: " + sum);
-console.log("Subtraction value is: " + subtraction);
-console.log("Multiplication value is: " + multiplication);
-console.log("Division value is: " + division);
-console.log("Remainder value is: " + remainder);
-console.log("Exponentiation value is: " + exponentiation);
+console.log('Sum value is: ' + sum);
+console.log('Subtraction value is: ' + subtraction);
+console.log('Multiplication value is: ' + multiplication);
+console.log('Division value is: ' + division);
+console.log('Remainder value is: ' + remainder);
+console.log('Exponentiation value is: ' + exponentiation);
