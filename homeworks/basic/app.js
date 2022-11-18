dd87f56d606e654891746bafb18f7015f9ef9988
Olga Vakulenko <vakulenkooya@gmail.com>
2022-11-18 14:22:40 +0200

    create basics.js

---
+++ homeworks/basic/app.js
@@ -0,0 +1,50 @@
+// First Instructions
+
+// First Program
+/*This is First My Program*/
+
+console.log("Hello, World!");
+console.log(124);
+
+// 2.variables
+
+var project = "Blog";
+let message = "Hello, Vova!";
+const course = "MA";
+
+course = "MAA";
+
+// message = 'Hello, MA';
+
+// console.log(message);
+// console.log(course);
+
+// 3. Data Types
+
+let isPrivateBoolean = true;
+let isPrivateString = "false";
+
+console.log(isPrivateBoolean);
+console.log(isPrivateString);
+
+// 4.Changing in Type
+
+const stringNumber = "101";
+const number = Number(stringNumber);
+
+console.log(typeof stringNumber);
+console.log(number);
+
+// const a = true;
+// const a = false;
+// const a = null;
+// const a = undefined;
+// const a = '101a';
+// const a = "hello";
+// const b = Number(a);
+
+const a = 901;
+const b = String(a);
+
+console.log(a, typeof a);
+console.log(b, typeof b);
---
+++ homeworks/olya.vakulenko_Oya-Ol/1-basics/basics.js
@@ -0,0 +1,15 @@
+// Math
+
+let firstNumber = 2;
+let secondNumber = 5;
+let thirdNumber = 10;
+
+console.log("Result:" + (firstNumber + secondNumber));
+console.log("Result:" + (thirdNumber - secondNumber));
+console.log("Result:" + (thirdNumber - firstNumber));
+console.log("Result:" + firstNumber * thirdNumber);
+console.log("Result:" + thirdNumber / secondNumber);
+console.log("Result:" + (secondNumber % thirdNumber));
+console.log("Result:" + firstNumber ** thirdNumber);
+
+// console.log("Hello, World!");
