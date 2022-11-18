e8afb7d173745ae5141f9ff051224b50e317c03f
Sobchenko-dev <sobchenkoaa@gmail.com>
2022-11-18 17:48:23 +0200

    fixed linter errors

--- homeworks/oleksandr.sobchenko_Sobchenko-dev/1-basics/basics.js
+++ homeworks/oleksandr.sobchenko_Sobchenko-dev/1-basics/basics.js
@@ -1,24 +1,23 @@
-let firstNumber = 10
-let secondNumber = 5
-let thirdNumber = 2
-
-const firstName = 'Oleksandr'
-const lastName = 'Sobchenko';
+const firstNumber=10;
+const secondNumber=5;
+const thirdNumber=2;
+const firstName='Oleksandr';
+const lastName='Sobchenko';
 
 //результати 
-const result0 = (firstNumber-secondNumber-thirdNumber)
-const result1 = secondNumber * thirdNumber
-const result2 = firstNumber / thirdNumber
-const result3 = firstNumber/thirdNumber
+const result0=(firstNumber-secondNumber-thirdNumber)
+const result1=secondNumber * thirdNumber
+const result2=firstNumber / thirdNumber
+const result3=firstNumber/thirdNumber
 
-// Додавання +
+//Додавання +
 console.log(firstName+' '+lastName)
 console.log(firstNumber+secondNumber)
 const firstNumberText=String (firstNumber)
 const secondNumberText=String(secondNumber)
 console.log(firstNumberText+secondNumberText) 
 
-// Віднімання -
+//Віднімання -
 console.log(firstNumber-secondNumber)
 console.log(result0)
 console.log(firstNumber-(secondNumber+thirdNumber))
