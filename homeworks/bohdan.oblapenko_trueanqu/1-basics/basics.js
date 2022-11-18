aeb3a2a20e8dbdf640784621714908109be54668
Bohdan Oblapenko <Bohdan.Oblapenko.-ND@disneystreaming.com>
2022-11-18 15:35:45 +0200

    1-basics: fix most linter errors

--- homeworks/bohdan.oblapenko_trueanqu/1-basics/basics.js
+++ homeworks/bohdan.oblapenko_trueanqu/1-basics/basics.js
@@ -1,61 +1,57 @@
 const numConst = 3;
 const anotherNumConst = 4;
 const stringNumber = '5';
-let accumulator;
-let stringVar = 'test';
-let anotherStringVar = 'me';
+const stringVar = 'test';
+const anotherStringVar = 'me';
 const boolTrue = true;
 const boolFalse = false;
-const undefinedVar = undefined;
-const nullVar = null;
 
-let addResult = numConst + anotherNumConst;
+const addResult = numConst + anotherNumConst;
 console.log('Expecting 7:', addResult);
 
-let substrResult = numConst - anotherNumConst;
+const substrResult = numConst - anotherNumConst;
 console.log('Expecting: -1:', substrResult);
 
-let multResult = numConst * anotherNumConst;
+const multResult = numConst * anotherNumConst;
 console.log('Expecting: 12', multResult);
 
-let divResult = numConst / anotherNumConst;
+const divResult = numConst / anotherNumConst;
 console.log('Expecting: 0.75', divResult);
 
-let modResult = anotherNumConst % numConst;
+const modResult = anotherNumConst % numConst;
 console.log('Expecting: 1', modResult);
 
-let powResult = numConst ** anotherNumConst;
+const powResult = numConst ** anotherNumConst;
 console.log('Expecting: 81', powResult);
 
 // automatic number to string conversion
-let stringPlusNumResult = stringNumber + numConst;
+const stringPlusNumResult = stringNumber + numConst;
 console.log('Expecting: 53 (string value)', stringPlusNumResult);
 
 // automatic string to number conversion (implicit, thus better to avoid)
-let stringMinusNumResult = stringNumber - numConst;
+const stringMinusNumResult = stringNumber - numConst;
 console.log('Expecting: 2', stringMinusNumResult);
 
 // automatic string to number conversion (implicit, thus better to avoid)
-let stringDivNumResult = stringNumber / anotherNumConst;
+const stringDivNumResult = stringNumber / anotherNumConst;
 console.log('Expecting: 1.25', stringDivNumResult);
 
 // invalid operation
-let stringDivStringResult = stringVar / anotherStringVar
+const stringDivStringResult = stringVar / anotherStringVar;
 console.log('Expecting: NaN', stringDivStringResult);
 
-
 // invalid operation
-let stringMultStringResult = stringVar * anotherStringVar
+const stringMultStringResult = stringVar * anotherStringVar;
 console.log('Expecting: NaN', stringDivStringResult);
 
 // implicit bool to number conversion. Is also the case for multiplication, division, power, modulus
-let boolFalseMinusNumResult = boolFalse - numConst;
+const boolFalseMinusNumResult = boolFalse - numConst;
 console.log('Expecting: -3', boolFalseMinusNumResult);
 
 // implicit bool to string conversion
-let boolTruePlusStringResult = boolTrue + stringVar;
+const boolTruePlusStringResult = boolTrue + stringVar;
 console.log('Expecting: truetest', boolTruePlusStringResult);
 
 // string concatenation using + operator
-let stringConcatStringResult = stringVar + ' ' + anotherStringVar;
+const stringConcatStringResult = stringVar + ' ' + anotherStringVar;
 console.log('Expecting: test me (string value):', stringConcatStringResult);
