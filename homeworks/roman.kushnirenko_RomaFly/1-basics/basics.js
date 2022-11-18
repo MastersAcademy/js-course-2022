e3aa351566609d4e0f60be8db2fb978aca1c129b
RomaFly <romfly7@gmail.com>
2022-11-18 13:51:13 +0200

    Changed name of variables and added variable for every operation

--- homeworks/roman.kushnirenko_RomaFly/1-basics/basics.js
+++ homeworks/roman.kushnirenko_RomaFly/1-basics/basics.js
@@ -1,11 +1,22 @@
-const numFirst = 15;
-const numSecond = 7;
+const firstNumber = 15;
+const secondNumber = 7;
 
 console.log('*** Арефметичні операції над двома числами: 15 і 7 ***');
 
-console.log(`Додавання: ${numFirst + numSecond}`);
-console.log(`Віднімання: ${numFirst - numSecond}`);
-console.log(`Множення: ${numFirst * numSecond}`);
-console.log(`Ділення: ${(numFirst / numSecond).toFixed(2)}`);
-console.log(`Остача від ділення: ${numFirst % numSecond}`);
-console.log(`Піднесення до степеня: ${numFirst ** numSecond}`);
+let adding = firstNumber + secondNumber;
+console.log(`Додавання: ${adding}`);
+
+let subtraction = firstNumber - secondNumber;
+console.log(`Віднімання: ${subtraction}`);
+
+let multiplication = firstNumber * secondNumber;
+console.log(`Множення: ${multiplication}`);
+
+let division = (firstNumber / secondNumber).toFixed(2);
+console.log(`Ділення: ${division}`);
+
+let remainderOfdivision = firstNumber % secondNumber;
+console.log(`Остача від ділення: ${remainderOfdivision}`);
+
+let exponentiation = firstNumber ** secondNumber;
+console.log(`Піднесення до степеня: ${exponentiation}`);
