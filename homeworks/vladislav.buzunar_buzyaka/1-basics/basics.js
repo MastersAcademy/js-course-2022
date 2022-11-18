fc62613b8b4380a08f33d42aa80e2526cd9c4efc
Vladislav Buzunar <98417449+buzyaka@users.noreply.github.com>
2022-11-18 02:52:16 +0200

    Add files via upload

--- homeworks/vladislav.buzunar_buzyaka/1-basics/basics.js
+++ homeworks/vladislav.buzunar_buzyaka/1-basics/basics.js
@@ -3,9 +3,9 @@
 const firstNumber = 7;
 const secondNumber = 2;
 const thirdNumber = 5;
-const myName = "Vlad";
-const firstSomeText = "I love Masters Academy";
-const secondSomeText = "I love JS";
+const myName = `Vlad`;
+const firstSomeText = `I love Masters Academy`;
+const secondSomeText = `I love JS`;
 
 // Actions numbers
 
@@ -28,11 +28,11 @@ console.log(divisAllNumbers);
 console.log(remainOfNumbers);
 
 // elevation to the square
-console.log("${firstNumber} squared is equal to ${Math.pow(firstNumber, 2)}"); // or **2, 3, 4...
-console.log("${secondNumber} squared is equal to ${Math.pow(secondNumber, 2)}"); // or **2, 3, 4...
-console.log("${thirdNumber} squared is equal to ${Math.pow(thirdNumber, 2)}"); // or **2, 3, 4...
+console.log(`${firstNumber} squared is equal to ${Math.pow(firstNumber, 2)}`); // or **2, 3, 4...
+console.log(`${secondNumber} squared is equal to ${Math.pow(secondNumber, 2)}`); // or **2, 3, 4...
+console.log(`${thirdNumber} squared is equal to ${Math.pow(thirdNumber, 2)}`); // or **2, 3, 4...
 
 // concatenation
 
-console.log("Hey, my name is ${myName}, i am 17y.o . " + firstSomeText + ". " + secondSomeText);
-console.log("First number is "  + firstNumber + ". And second number is " + secondNumber + ". And third number is " + thirdNumber);
+console.log(`Hey, my name is , i am 17y.o . ` + firstSomeText + `. ` + secondSomeText);
+console.log(`First number is `  + firstNumber + `. And second number is ` + secondNumber + `. And third number is ` + thirdNumber);
