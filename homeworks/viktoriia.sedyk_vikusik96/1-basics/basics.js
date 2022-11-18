c62ede7d707065f5483dde1d8fe3439c57baeeb0
vikusik <dp130396kvv@gmail.com>
2022-11-18 18:08:50 +0200

    added semicolons

--- homeworks/viktoriia.sedyk_vikusik96/1-basics/basics.js
+++ homeworks/viktoriia.sedyk_vikusik96/1-basics/basics.js
@@ -1,9 +1,9 @@
-const firstNumber = 10
-const secondNumber = 4
+const firstNumber = 10;
+const secondNumber = 4;
 
-console.log(firstNumber + secondNumber)
-console.log(firstNumber - secondNumber)
-console.log(firstNumber * secondNumber)
-console.log(firstNumber / secondNumber)
-console.log(firstNumber % secondNumber)
-console.log(firstNumber ** secondNumber)
+console.log(firstNumber + secondNumber);
+console.log(firstNumber - secondNumber);
+console.log(firstNumber * secondNumber);
+console.log(firstNumber / secondNumber);
+console.log(firstNumber % secondNumber);
+console.log(firstNumber ** secondNumber);
