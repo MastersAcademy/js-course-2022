043ab5ef757897e8437b5824b490bec0743c0a35
Bohdan Oblapenko <Bohdan.Oblapenko.-ND@disneystreaming.com>
2022-11-18 15:32:19 +0200

    1-basics: add some boolean and string operations

--- homeworks/bohdan.oblapenko_trueanqu/1-basics/basics.js
+++ homeworks/bohdan.oblapenko_trueanqu/1-basics/basics.js
@@ -4,6 +4,10 @@ const stringNumber = '5';
 let accumulator;
 let stringVar = 'test';
 let anotherStringVar = 'me';
+const boolTrue = true;
+const boolFalse = false;
+const undefinedVar = undefined;
+const nullVar = null;
 
 let addResult = numConst + anotherNumConst;
 console.log('Expecting 7:', addResult);
@@ -33,4 +37,25 @@ console.log('Expecting: 2', stringMinusNumResult);
 
 // automatic string to number conversion (implicit, thus better to avoid)
 let stringDivNumResult = stringNumber / anotherNumConst;
-console.warn('Expecting: 1.25', stringDivNumResult);
+console.log('Expecting: 1.25', stringDivNumResult);
+
+// invalid operation
+let stringDivStringResult = stringVar / anotherStringVar
+console.log('Expecting: NaN', stringDivStringResult);
+
+
+// invalid operation
+let stringMultStringResult = stringVar * anotherStringVar
+console.log('Expecting: NaN', stringDivStringResult);
+
+// implicit bool to number conversion. Is also the case for multiplication, division, power, modulus
+let boolFalseMinusNumResult = boolFalse - numConst;
+console.log('Expecting: -3', boolFalseMinusNumResult);
+
+// implicit bool to string conversion
+let boolTruePlusStringResult = boolTrue + stringVar;
+console.log('Expecting: truetest', boolTruePlusStringResult);
+
+// string concatenation using + operator
+let stringConcatStringResult = stringVar + ' ' + anotherStringVar;
+console.log('Expecting: test me (string value):', stringConcatStringResult);
