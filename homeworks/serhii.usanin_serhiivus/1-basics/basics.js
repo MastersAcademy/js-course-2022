ce4b32616677cee845174a9557a75adbdf3e659b
seghiivus <serhiivus@ukr.net>
2022-11-18 23:01:18 +0200

    1-basics

---
+++ homeworks/serhii.usanin_serhiivus/1-basics/basics.js
@@ -0,0 +1,106 @@
+const firstNumber = 15;
+const secondNumber = 17;
+
+var sum = firstNumber + secondNumber;
+console.log(
+    "First number = ",
+    firstNumber,
+    " Second number =",
+    secondNumber,
+    " Sum = ",
+    sum,
+    typeof sum
+);
+
+var difference = firstNumber - secondNumber;
+console.log(
+    "First number = ",
+    firstNumber,
+    " Second number =",
+    secondNumber,
+    " Difference = ",
+    difference,
+    typeof difference
+);
+
+var product = firstNumber * secondNumber;
+console.log(
+    "First number = ",
+    firstNumber,
+    " Second number =",
+    secondNumber,
+    " Product = ",
+    product,
+    typeof product
+);
+
+var fraction = firstNumber / secondNumber;
+console.log(
+    "First number = ",
+    firstNumber,
+    " Second number =",
+    secondNumber,
+    " Fraction = ",
+    fraction,
+    typeof fraction
+);
+
+var renOfDivision = firstNumber % secondNumber;
+console.log(
+    "First number = ",
+    firstNumber,
+    " Second number =",
+    secondNumber,
+    " Remainder of division = ",
+    renOfDivision,
+    typeof renOfDivision
+);
+
+const degree = 3;
+var expFirst = firstNumber ** degree;
+console.log(
+    "First number = ",
+    firstNumber,
+    " Degree =",
+    degree,
+    " Exponentiate = ",
+    expFirst,
+    typeof expFirst
+);
+
+var divByZero = firstNumber / 0;
+console.log(
+    "First number = ",
+    firstNumber,
+
+    " Division by zero = ",
+    divByZero,
+    typeof divByZero
+);
+
+var divByString = firstNumber / "ss";
+console.log(
+    "First number = ",
+    firstNumber,
+
+    " Division by string = ",
+    divByString,
+    typeof divByString
+);
+
+var undef;
+console.log(" Undefinded = ", undef, typeof undef);
+
+var exampNull = null;
+console.log(" null = ", exampNull, typeof exampNull);
+
+const firstString = "Hello ";
+const secondString = "world ";
+
+let concat = firstString + secondString;
+console.log(" Concat = ", concat, typeof concat);
+
+let diff = firstString - secondString;
+console.log(" diff = ", diff, typeof diff);
+
+console.log("undef:", typeof kjf)
