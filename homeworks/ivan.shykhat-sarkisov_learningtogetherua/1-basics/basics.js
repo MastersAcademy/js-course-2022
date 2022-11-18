f1a04afc07960cfab66c38d798121b409056587e
learningtogetherua <learningtogetherua@gmail.com>
2022-11-18 15:08:02 +0200

    1 hmw add template string

--- homeworks/ivan.shykhat-sarkisov_learningtogetherua/1-basics/basics.js
+++ homeworks/ivan.shykhat-sarkisov_learningtogetherua/1-basics/basics.js
@@ -1,20 +1,20 @@
 const courseVendor = 'Masters Academy';
-let courseUsersInChat;
+let courseUsersInChat = 0;
 const courseUsersMale = 33;
 const courseUsersFemale = 9;
 
 courseUsersInChat = courseUsersFemale + courseUsersMale;
-console.log('Кількість користувачів у чаті курсу від ' + courseVendor + ' дорівнює: ' + courseUsersInChat);
+console.log(`Кількість користувачів у чаті курсу від ${courseVendor} дорівнює: ${courseUsersInChat}`);
 
 console.log(`Кількість дівчаток у чаті курсу на ${courseUsersMale - courseUsersFemale} менша за хлопчиків`);
 
-let minutesQuantityOfCourse = 60 * 2 * 8;
-console.log('Тривалість курсу у хвилинах дорівнює: ' + minutesQuantityOfCourse);
+const minutesQuantityOfCourse = 60 * 2 * 8;
+console.log(`Тривалість курсу у хвилинах дорівнює: ${minutesQuantityOfCourse}`);
 
-let hoursQuantityOfCourse = minutesQuantityOfCourse / 60;
-console.log('Тривалість курсу у годинах дорівнює: ' + hoursQuantityOfCourse);
+const hoursQuantityOfCourse = minutesQuantityOfCourse / 60;
+console.log(`Тривалість курсу у годинах дорівнює: ${hoursQuantityOfCourse}`);
 
-console.log('Кількість хлопчиків у чаті курсу непарна. Це відомо з того, що остача від ділення їх кількості (' + courseUsersMale + ') на 2 дорівнює: ' + (courseUsersMale % 2));
+console.log(`Кількість хлопчиків у чаті курсу непарна. Це відомо з того, що остача від ділення їх кількості ${courseUsersMale} на 2 дорівнює: ${courseUsersMale % 2}`);
 
-console.log('Кількість дівчаток у чаті курсу (' + courseUsersFemale + ') є числом 3 піднесеним до степеня 2: ' + (3 ** 2));
+console.log(`Кількість дівчаток у чаті курсу (${courseUsersFemale}) є числом 3 піднесеним до степеня 2: ${3 ** 2}`);
 
