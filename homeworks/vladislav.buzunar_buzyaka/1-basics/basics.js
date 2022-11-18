ca24eecbf0c34b9a9e2ecce41bb0297378bf9b6a
Vladislav Buzunar <98417449+buzyaka@users.noreply.github.com>
2022-11-18 02:34:57 +0200

    Add files via upload

--- homeworks/vladislav.buzunar_buzyaka/1-basics/basics.js
+++ homeworks/vladislav.buzunar_buzyaka/1-basics/basics.js
@@ -1,33 +1,39 @@
-// Variable numbers 
-const firstNumber = 7; 
-const secondNumber = 2; 
-const thirdNumber = 5; 
-const myName = `Vlad`; 
-const firstSomeText = `I love Masters Academy`; 
-const secondSomeText = `I love JS`; 
- 
-// Actions numbers 
-const sumAllNumbers = firstNumber + secondNumber + thirdNumber; 
-const multAllNumbers = firstNumber * secondNumber * thirdNumber; 
-const divisAllNumbers = firstNumber / secondNumber / thirdNumber; 
-const remainOfNumbers = firstNumber % secondNumber; 
-// (firstNumber % thirdNumber); 
-// (secondNumber % firstNumber); 
-// (secondNumber % thirdNumber); 
-// (thirdNumber % firstNumber); 
-// (thirdNumber % secondNumber); 
- 
-// Output to the Console 
+// Variable numbers
+
+const firstNumber = 7;
+const secondNumber = 2;
+const thirdNumber = 5;
+const myName = `Vlad`;
+const firstSomeText = `I love Masters Academy`;
+const secondSomeText = `I love JS`;
+
+// Actions numbers
+
+const sumAllNumbers = firstNumber+secondNumber+thirdNumber;
+const multAllNumbers = firstNumber*secondNumber*thirdNumber;
+const divisAllNumbers = firstNumber/secondNumber/thirdNumber;
+const remainOfNumbers = firstNumber%secondNumber;
+
+// (firstNumber % thirdNumber);
+// (secondNumber % firstNumber);
+// (secondNumber % thirdNumber);
+// (thirdNumber % firstNumber);
+// (thirdNumber % secondNumber);
+
+// Output to the Console
+
 console.log(sumAllNumbers);
-console.log(multAllNumbers); 
-console.log(divisAllNumbers); 
-console.log(remainOfNumbers); 
- 
-// elevation to the square 
-console.log(`${firstNumber} squared is equal to ${Math.pow(firstNumber, 2)}`); // or **2, 3, 4... 
-console.log(`${secondNumber} squared is equal to ${Math.pow(secondNumber, 2)}`); // or **2, 3, 4... 
-console.log(`${thirdNumber} squared is equal to ${Math.pow(thirdNumber, 2)}`); // or **2, 3, 4... 
- 
-// concatenation 
-console.log(`Hey, my name is ${myName}, i am 17y.o . ` + firstSomeText + '. ' + secondSomeText); 
-console.log(`First number is `  + firstNumber + `. And second number is `  + secondNumber + `. And third number is ` + thirdNumber);
+console.log(multAllNumbers);
+console.log(divisAllNumbers);
+console.log(remainOfNumbers);
+
+// elevation to the square
+
+console.log(`${firstNumber} squared is equal to ${Math.pow(firstNumber, 2)}`); // or **2, 3, 4...
+console.log(`${secondNumber} squared is equal to ${Math.pow(secondNumber, 2)}`); // or **2, 3, 4...
+console.log(`${thirdNumber} squared is equal to ${Math.pow(thirdNumber, 2)}`); // or **2, 3, 4...
+
+// concatenation
+
+console.log(`Hey, my name is ${myName}, i am 17y.o . `+firstSomeText+'. '+secondSomeText);
+console.log(`First number is `  + firstNumber + `. And second number is `+ secondNumber+ `. And third number is `+thirdNumber);
