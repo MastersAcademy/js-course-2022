7e30ab804df0547ef8b0809a49eda9252cbb4854
KaterinaTkach <kassy1606@gmail.com>
2022-11-18 20:31:53 +0200

    added 1-basics directory and basics js HW, modified o-intro (README.md) HW

---
+++ homeworks/katerina.tkach_KaterinaTkach/1-basics/basics.js
@@ -0,0 +1,47 @@
+// Створити декілька довільних переміних в файлі і далі виконати з ними наступні арефметичні операції:
+
+// Додавання +
+let num1 = 10;
+let num2 = 20;
+console.log(num1 + num2);
+console.log('^^^this is Додавання + example^^^');
+
+// Віднімання -
+console.log(num1 - num2);
+console.log('^^^this is Віднімання - example^^^');
+
+// Множення *
+console.log(num1 * num2);
+console.log('^^^this is Множення * example^^^');
+
+// Ділення /
+console.log(num1 / num2);
+console.log('^^^this is Ділення / example^^^');
+
+// Остача від ділення %
+let num3 = 7;
+let num4 = 2;
+console.log(num3 % num4);
+console.log('^^^^this is Остача від ділення % example^^^');
+
+// Піднесення до степеня **
+console.log(num3 ** num4);
+console.log('^^^^this is Піднесення до степеня ** example^^^');
+
+// Операція конкатенація =)
+let firstString = 'Biba';
+let secondString = 'Boba';
+console.log(firstString + " & " + secondString  + " together forever " );
+console.log("^^^^this is Операція конкатенація =) example^^^ ");
+
+// Операція з додавання "Стрінгів" з числовим значенням 
+let firstString1 = '1';
+let secondString1 = '100';
+console.log(firstString1 + secondString1);
+console.log('^^^^this is додавання "Стрінгів" з числовим значенням example^^^');
+
+// Операція з додавання "Стрінгів " з числовим значенням по багатому
+let firstString911=  String(999);
+let secondString911 = String(1);
+console.log(firstString911 + secondString911);
+console.log('^^^^this is додавання "Стрінгів" з числовим значенням  по багатому example^^^');
