e311284270d2156caed3cc6304086bcb5d777463
dmytro_barchan <timonbarchan@gmail.com>
2022-11-18 22:53:59 +0200

    HM2 implementation

---
+++ homeworks/dmytro.barchan_barchanbarchan/1-basics/basics.js
@@ -0,0 +1,74 @@
+// Number
+let firstNumber = 9;
+let secondNumder = 3;
+
+let result1 = firstNumber + secondNumder;
+let result2 = firstNumber - secondNumder;
+let result3 = firstNumber * secondNumder;
+let result4 = firstNumber / secondNumder;
+let result5 = firstNumber % secondNumder;
+let result6 = firstNumber ** secondNumder;
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
+console.log(result5);
+console.log(result6);
+
+// String
+
+let firstString = "1991";
+let secondString = "2022";
+
+let result7 = firstString + secondString;
+let result8 = firstString - secondString;
+let result9 = firstString * secondString;
+let result10 = firstString / secondString;
+let result11= firstString % secondString;
+let result12= firstString ** secondString;
+
+console.log(result7, typeof(result7));
+console.log(result8, typeof(result8));
+console.log(result9, typeof(result9));
+console.log(result10, typeof(result10));
+console.log(result11, typeof(result11));
+console.log(result12, typeof(result12));
+
+//Object
+
+let firstOject = null;
+let secondOject = undefined;
+
+let result13 = (firstOject + firstOject);
+let result14 = (firstOject - firstOject);
+let result15 = (firstOject * firstOject);
+let result16 = (firstOject / firstOject);
+let result17 = (firstOject % firstOject);
+let result18 = (firstOject ** firstOject);
+
+
+console.log(result13);
+console.log(result14);
+console.log(result15);
+console.log(result16);
+console.log(result17);
+console.log(result18);
+
+//Number, String
+
+let result19 = (firstOject + firstOject);
+let result20 = (firstOject - firstOject);
+let result21 = (firstOject * firstOject);
+let result22 = (firstOject / firstOject);
+let result23 = (firstOject % firstOject);
+let result24 = (firstOject ** firstOject);
+
+console.log(result19, typeof(result19));
+console.log(result20, typeof(result20));
+console.log(result21, typeof(result21));
+console.log(result22, typeof(result22));
+console.log(result23, typeof(result23));
+console.log(result24, typeof(result24));
+
+console.log(typeof(Number(firstOject)));
