8ed0207d51099624fed4062e07f5615388de2e62
irynagonzalez <irynagonsales@gmail.com>
2022-11-19 10:08:44 +0100

    task 01

---
+++ homeworks/iryna.gonzalez_irynagonzalez/1-basics/basics.js
@@ -0,0 +1,20 @@
+let argument1 = 12;
+let argument2 = 2.5;
+
+let summ = (argument1 + argument2);
+console.log("sum="+ summ);
+
+let sub = (argument1 - argument2);
+console.log("sub="+ sub);
+    
+let mul = (argument1 * argument2);
+console.log("mul="+ mul);
+
+let div = (argument1 / argument2);
+console.log("div=" + div);
+
+let rem = (argument1 % argument2);
+console.log ("rem=" + rem);
+
+let pow = (argument1 ** argument2);
+console.log("pow=" + pow);
