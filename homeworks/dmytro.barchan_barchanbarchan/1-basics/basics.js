cf0359d92613bf063637811dd6b646b3af6f5d59
dmytro_barchan <timonbarchan@gmail.com>
2022-11-19 11:26:53 +0200

    HM2 implementation

--- homeworks/dmytro.barchan_barchanbarchan/1-basics/basics.js
+++ homeworks/dmytro.barchan_barchanbarchan/1-basics/basics.js
@@ -1,13 +1,13 @@
 // Number
-let firstNumber = 9;
-let secondNumder = 3;
+const firstNumber = 9;
+const secondNumder = 3;
 
-let result1 = firstNumber + secondNumder;
-let result2 = firstNumber - secondNumder;
-let result3 = firstNumber * secondNumder;
-let result4 = firstNumber / secondNumder;
-let result5 = firstNumber % secondNumder;
-let result6 = firstNumber ** secondNumder;
+const result1 = firstNumber + secondNumder;
+const result2 = firstNumber - secondNumder;
+const result3 = firstNumber * secondNumder;
+const result4 = firstNumber / secondNumder;
+const result5 = firstNumber % secondNumder;
+const result6 = firstNumber ** secondNumder;
 
 console.log(result1);
 console.log(result2);
@@ -18,35 +18,34 @@ console.log(result6);
 
 // String
 
-let firstString = "1991";
-let secondString = "2022";
+const firstString = '1991';
+const secondString = '2022';
 
-let result7 = firstString + secondString;
-let result8 = firstString - secondString;
-let result9 = firstString * secondString;
-let result10 = firstString / secondString;
-let result11= firstString % secondString;
-let result12= firstString ** secondString;
+const result7 = firstString + secondString;
+const result8 = firstString - secondString;
+const result9 = firstString * secondString;
+const result10 = firstString / secondString;
+const result11 = firstString % secondString;
+const result12 = firstString ** secondString;
 
-console.log(result7, typeof(result7));
-console.log(result8, typeof(result8));
-console.log(result9, typeof(result9));
-console.log(result10, typeof(result10));
-console.log(result11, typeof(result11));
-console.log(result12, typeof(result12));
+console.log(result7, typeof result7);
+console.log(result8, typeof result8);
+console.log(result9, typeof result9);
+console.log(result10, typeof result10);
+console.log(result11, typeof result11);
+console.log(result12, typeof result12);
 
-//Object
+// Object
 
-let firstOject = null;
-let secondOject = undefined;
+const firstOject = null;
+const secondOject = '';
 
-let result13 = (firstOject + firstOject);
-let result14 = (firstOject - firstOject);
-let result15 = (firstOject * firstOject);
-let result16 = (firstOject / firstOject);
-let result17 = (firstOject % firstOject);
-let result18 = (firstOject ** firstOject);
-
+const result13 = (firstOject + secondOject);
+const result14 = (firstOject - secondOject);
+const result15 = (firstOject * secondOject);
+const result16 = (firstOject / secondOject);
+const result17 = (firstOject % secondOject);
+const result18 = (firstOject ** secondOject);
 
 console.log(result13);
 console.log(result14);
@@ -55,20 +54,20 @@ console.log(result16);
 console.log(result17);
 console.log(result18);
 
-//Number, String
+// Number, String
 
-let result19 = (firstOject + firstOject);
-let result20 = (firstOject - firstOject);
-let result21 = (firstOject * firstOject);
-let result22 = (firstOject / firstOject);
-let result23 = (firstOject % firstOject);
-let result24 = (firstOject ** firstOject);
+const result19 = (firstOject + firstString);
+const result20 = (firstOject - firstString);
+const result21 = (firstOject * firstString);
+const result22 = (firstOject / firstString);
+const result23 = (firstOject % firstString);
+const result24 = (firstOject ** firstString);
 
-console.log(result19, typeof(result19));
-console.log(result20, typeof(result20));
-console.log(result21, typeof(result21));
-console.log(result22, typeof(result22));
-console.log(result23, typeof(result23));
-console.log(result24, typeof(result24));
+console.log(result19, typeof result19);
+console.log(result20, typeof result20);
+console.log(result21, typeof result21);
+console.log(result22, typeof result22);
+console.log(result23, typeof result23);
+console.log(result24, typeof result24);
 
-console.log(typeof(Number(firstOject)));
+console.log(typeof Number(firstOject));
