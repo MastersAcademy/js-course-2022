22ccb96458f9428a45d04011102524fc100b19dc
KaterinaTkach <kassy1606@gmail.com>
2022-11-21 12:18:00 +0200

    fixed eslint errors

--- homeworks/katerina.tkach_KaterinaTkach/1-basics/basics.js
+++ homeworks/katerina.tkach_KaterinaTkach/1-basics/basics.js
@@ -1,8 +1,6 @@
-// Створити декілька довільних переміних в файлі і далі виконати з ними наступні арефметичні операції:
-
 // Додавання +
-let num1 = 10;
-let num2 = 20;
+const num1 = 10;
+const num2 = 20;
 console.log(num1 + num2);
 console.log('^^^this is Додавання + example^^^');
 
@@ -19,8 +17,8 @@ console.log(num1 / num2);
 console.log('^^^this is Ділення / example^^^');
 
 // Остача від ділення %
-let num3 = 7;
-let num4 = 2;
+const num3 = 7;
+const num4 = 2;
 console.log(num3 % num4);
 console.log('^^^^this is Остача від ділення % example^^^');
 
@@ -29,19 +27,19 @@ console.log(num3 ** num4);
 console.log('^^^^this is Піднесення до степеня ** example^^^');
 
 // Операція конкатенація =)
-let firstString = 'Biba';
-let secondString = 'Boba';
-console.log(firstString + " & " + secondString  + " together forever " );
-console.log("^^^^this is Операція конкатенація =) example^^^ ");
+const firstString = 'Biba';
+const secondString = 'Boba';
+console.log(`${firstString} &  ${secondString}   together forever `);
+console.log('^^^^this is Операція конкатенація =) example^^^ ');
 
-// Операція з додавання "Стрінгів" з числовим значенням 
-let firstString1 = '1';
-let secondString1 = '100';
+// Операція з додавання "Стрінгів" з числовим значенням
+const firstString1 = '1';
+const secondString1 = '100';
 console.log(firstString1 + secondString1);
 console.log('^^^^this is додавання "Стрінгів" з числовим значенням example^^^');
 
 // Операція з додавання "Стрінгів " з числовим значенням по багатому
-let firstString911=  String(999);
-let secondString911 = String(1);
+const firstString911 = String(999);
+const secondString911 = String(1);
 console.log(firstString911 + secondString911);
 console.log('^^^^this is додавання "Стрінгів" з числовим значенням  по багатому example^^^');
