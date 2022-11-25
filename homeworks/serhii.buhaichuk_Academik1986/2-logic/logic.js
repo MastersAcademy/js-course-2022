481744273d4e9a3c55db17e6586069a266adc40b
Academik1986 <servega.ua@gmail.com>
2022-11-25 04:52:11 +0200

    Calculator-8

--- homeworks/serhii.buhaichuk_Academik1986/2-logic/logic.js
+++ homeworks/serhii.buhaichuk_Academik1986/2-logic/logic.js
@@ -4,19 +4,19 @@ const firstNumber = 11;
 const secondNumber = 2;
 const operation = '+';
 if (operation === '+') {
-   console.log(firstNumber + secondNumber);
+    console.log(firstNumber + secondNumber);
 } else if (operation === '-') {
-   console.log(firstNumber - secondNumber);
+    console.log(firstNumber - secondNumber);
 } else if (operation === '*') {
-   console.log(firstNumber * secondNumber);
+    console.log(firstNumber * secondNumber);
 } else if (operation === '/') {
-   console.log(firstNumber / secondNumber);
+    console.log(firstNumber / secondNumber);
 } else if (operation === '%') {
-   console.log(firstNumber % secondNumber);
+    console.log(firstNumber % secondNumber);
 } else if (operation === '**') {
-   console.log(firstNumber ** secondNumber);
+    console.log(firstNumber ** secondNumber);
 } else {
-   console.log(0);
+    console.log(0);
 }
 
 // Калькулятор switch case *
@@ -25,25 +25,25 @@ const firstNumber1 = 15;
 const secondNumber2 = 4;
 const operator = '+';
 switch (operator) {
-   case '+':
-      console.log(firstNumber1 + secondNumber2);
-      break;
-   case '-':
-      console.log(firstNumber1 - secondNumber2);
-      break;
-   case '*':
-      console.log(firstNumber1 * secondNumber2);
-      break;
-   case '/':
-      console.log(firstNumber1 / secondNumber2);
-      break;
-   case '%':
-      console.log(firstNumber1 % secondNumber2);
-      break;
-   case '**':
-      console.log(firstNumber1 ** secondNumber2);
-      break;
-   default:
-      console.log(0);
-      break;
+    case '+':
+        console.log(firstNumber1 + secondNumber2);
+        break;
+    case '-':
+        console.log(firstNumber1 - secondNumber2);
+        break;
+    case '*':
+        console.log(firstNumber1 * secondNumber2);
+        break;
+    case '/':
+        console.log(firstNumber1 / secondNumber2);
+        break;
+    case '%':
+        console.log(firstNumber1 % secondNumber2);
+        break;
+    case '**':
+        console.log(firstNumber1 ** secondNumber2);
+        break;
+    default:
+        console.log(0);
+        break;
 }
