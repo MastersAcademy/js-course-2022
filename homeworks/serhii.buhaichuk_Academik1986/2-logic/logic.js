4075e98f8e31dca4b0aa3bea2945345db53b7af7
Academik1986 <servega.ua@gmail.com>
2022-11-25 03:31:21 +0200

    Calculator-7

--- homeworks/serhii.buhaichuk_Academik1986/2-logic/logic.js
+++ homeworks/serhii.buhaichuk_Academik1986/2-logic/logic.js
@@ -3,38 +3,19 @@
 const firstNumber = 11;
 const secondNumber = 2;
 const operation = '+';
-
 if (operation === '+') {
    console.log(firstNumber + secondNumber);
-
-}
-
-else if (operation === '-') {
+} else if (operation === '-') {
    console.log(firstNumber - secondNumber);
-
-}
-
-else if (operation === '*') {
+} else if (operation === '*') {
    console.log(firstNumber * secondNumber);
-
-}
-
-else if (operation === '/') {
+} else if (operation === '/') {
    console.log(firstNumber / secondNumber);
-
-}
-
-else if (operation === '%') {
+} else if (operation === '%') {
    console.log(firstNumber % secondNumber);
-
-}
-
-else if (operation === '**') {
+} else if (operation === '**') {
    console.log(firstNumber ** secondNumber);
-
-}
-
-else {
+} else {
    console.log(0);
 }
 
@@ -43,33 +24,25 @@ else {
 const firstNumber1 = 15;
 const secondNumber2 = 4;
 const operator = '+';
-
 switch (operator) {
-
    case '+':
       console.log(firstNumber1 + secondNumber2);
       break;
-
    case '-':
       console.log(firstNumber1 - secondNumber2);
       break;
-
    case '*':
       console.log(firstNumber1 * secondNumber2);
       break;
-
    case '/':
       console.log(firstNumber1 / secondNumber2);
       break;
-
    case '%':
       console.log(firstNumber1 % secondNumber2);
       break;
-
    case '**':
       console.log(firstNumber1 ** secondNumber2);
       break;
-
    default:
       console.log(0);
       break;
