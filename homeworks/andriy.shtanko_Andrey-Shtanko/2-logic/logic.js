b148115bbe46fa9a4419ce4cf52b7299b13fb739
Andrey-Shtanko <97941755+Andrey-Shtanko@users.noreply.github.com>
2022-11-25 14:27:47 +0200

    Create logic.js

---
+++ homeworks/andriy.shtanko_Andrey-Shtanko/2-logic/logic.js
@@ -0,0 +1,26 @@
+const firstNumber = 20;
+const secondNumber = 22;
+const operation = "*";
+
+const calculate = (a, b, operation) => {
+    switch (operation) {
+        case "+":
+            console.log(a + b);
+            break;
+        case "-":
+            console.log(a - b);
+            break;
+        case "*":
+            console.log(a * b);
+            break;
+        case "/":
+            console.log(a / b);
+            break;
+
+        default:
+            console.log(`Wrong operator! Please change.`);
+            break;
+    }
+};
+
+calculate(firstNumber, secondNumber, operation);
