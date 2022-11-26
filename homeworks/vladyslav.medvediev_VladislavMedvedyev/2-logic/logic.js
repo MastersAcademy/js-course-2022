f78742d4eef4c8c2e2ba8abd433cf6ef5ca0063f
Vladislav.Medvedyev <vlad.medvedyev@gmail.com>
2022-11-26 02:19:33 +0200

    2-logical

---
+++ homeworks/vladyslav.medvediev_VladislavMedvedyev/2-logic/logic.js
@@ -0,0 +1,27 @@
+const firstNumber = 20;
+const secondNumber = 22;
+const operation = '*';
+
+function calc() {
+    switch (operation) {
+        case '*':
+            console.log(firstNumber * secondNumber);
+            break;
+        case '/':
+            console.log(firstNumber / secondNumber);
+            break;
+        case '+':
+            console.log(firstNumber + secondNumber);
+            break;
+        case '-':
+            console.log(firstNumber - secondNumber);
+            break;
+        case '%':
+            console.log(firstNumber % secondNumber);
+            break;
+        default:
+            console.log("const operation is not valid");
+    }
+}
+
+calc();
