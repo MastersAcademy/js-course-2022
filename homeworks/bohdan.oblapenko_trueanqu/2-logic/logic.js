b614a6a02b630603d6e50b02a05d12139410e90a
Bohdan Oblapenko <bogdan.oblapenko@gmail.com>
2022-11-27 16:23:13 +0200

    2-logic: use if and switch to perform a calculation

---
+++ homeworks/bohdan.oblapenko_trueanqu/2-logic/logic.js
@@ -0,0 +1,37 @@
+const firstNumber = 20;
+const secondNumber = 22;
+const operation = '+';
+
+const notSupportedMessage = `Operation '${operation}' is not yet supported, sorry!`;
+
+console.log(`Calculating ${firstNumber} ${operation} ${secondNumber} using if statement:`);
+
+if (operation === '+')
+    console.log(firstNumber + secondNumber);
+else if (operation === '-')
+    console.log(firstNumber - secondNumber);
+else if (operation === '*')
+    console.log(firstNumber * secondNumber);
+else if (operation === '/')
+    console.log(firstNumber / secondNumber);
+else
+    console.log(notSupportedMessage);
+
+console.log(`Calculating ${firstNumber} ${operation} ${secondNumber} using switch statement:`);  
+
+switch (operation) {
+    case '+':
+        console.log(firstNumber + secondNumber);
+        break;
+    case '-':
+        console.log(firstNumber - secondNumber);
+        break;
+    case '*':
+        console.log(firstNumber * secondNumber);
+        break;
+    case '/':
+        console.log(firstNumber / secondNumber);
+        break;
+    default:
+        console.log(notSupportedMessage);
+}
