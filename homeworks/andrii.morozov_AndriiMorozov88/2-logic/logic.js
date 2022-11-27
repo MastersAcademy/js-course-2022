19269420c5a8231f51c69957ac7faa9d5c100895
Andrii Morozov <slon88888@ukr.net>
2022-11-27 12:31:56 +0200

    fixed linter mistakes

--- homeworks/andrii.morozov_AndriiMorozov88/2-logic/logic.js
+++ homeworks/andrii.morozov_AndriiMorozov88/2-logic/logic.js
@@ -1,17 +1,17 @@
 const firstNumber = 20;
 const secondNumber = 22;
 const operation = '/';
-switch(operation) {
+switch (operation) {
     case '*':
         console.log(firstNumber * secondNumber);
         break;
     case '+':
         console.log(firstNumber + secondNumber);
         break;
-        case '-':
-            console.log(firstNumber - secondNumber);
-            break;
-        case '/':
-            console.log(firstNumber / secondNumber);
-            break;
+    case '-':
+        console.log(firstNumber - secondNumber);
+        break;
+    default:
+        console.log(firstNumber / secondNumber);
+        break;
 }
