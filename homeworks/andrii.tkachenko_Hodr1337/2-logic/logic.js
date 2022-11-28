b542fbfd0241db137bdda8e393e4b2a6ccf27af5
judge-7757 <hodr1337@ukr.net>
2022-11-27 21:29:19 -0500

    HW 2

---
+++ homeworks/andrii.tkachenko_Hodr1337/2-logic/logic.js
@@ -0,0 +1,25 @@
+const firstNumber = 20;
+const secondNumber = 22;
+const operation = '*';
+switch (operation) {
+    case '*':
+        console.log(firstNumber * secondNumber);
+        break;
+    case '/':
+        console.log(firstNumber / secondNumber);
+        break;
+    case '+':
+        console.log(firstNumber + secondNumber);
+        break;
+    case '-':
+        console.log(firstNumber - secondNumber);
+        break;
+    case '**':
+        console.log(firstNumber ** secondNumber);
+        break;
+    case '%':
+        console.log(firstNumber % secondNumber);
+        break;
+    default:
+        console.log('Something went wrong');
+}
