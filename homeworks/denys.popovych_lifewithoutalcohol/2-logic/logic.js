ccd7f19762b0b664f45de93427301f65d1d566cb
lifewithoutalcohol <108883301+lifewithoutalcohol@users.noreply.github.com>
2022-12-01 00:48:42 +0200

    made prompt

--- homeworks/denys.popovych_lifewithoutalcohol/2-logic/logic.js
+++ homeworks/denys.popovych_lifewithoutalcohol/2-logic/logic.js
@@ -1,47 +1,72 @@
-const firstNumber = 20;
-const secondNumber = 22;
-const operation = '*';
+let firstNumber = '';
+let secondNumber = '';
+let operation = '*';
 let valueIfCalc = '';
 let valueSwitchCalc = '';
 
-if (operation === '+') {
-    valueIfCalc = firstNumber + secondNumber;
-} else if (operation === '-') {
-    valueIfCalc = firstNumber - secondNumber;
-} else if (operation === '*') {
-    valueIfCalc = firstNumber * secondNumber;
-} else if (operation === '/') {
-    valueIfCalc = firstNumber / secondNumber;
-} else if (operation === '**') {
-    valueIfCalc = firstNumber ** secondNumber;
-} else if (operation === '%') {
-    valueIfCalc = firstNumber % secondNumber;
+function calculateByIfElse() {
+    if (operation === '+') {
+        valueIfCalc = firstNumber + secondNumber;
+    } else if (operation === '-') {
+        valueIfCalc = firstNumber - secondNumber;
+    } else if (operation === '*') {
+        valueIfCalc = firstNumber * secondNumber;
+    } else if (operation === '/') {
+        valueIfCalc = firstNumber / secondNumber;
+    } else if (operation === '**') {
+        valueIfCalc = firstNumber ** secondNumber;
+    } else if (operation === '%') {
+        valueIfCalc = firstNumber % secondNumber;
+    }
+    console.log(`Calculated through the conditional operator 'if', 'else if' and value = ${valueIfCalc}`);
+    return valueIfCalc;
+}
+function calculateBySwitchCase() {
+    switch (operation) {
+        case '+':
+            valueSwitchCalc = firstNumber + secondNumber;
+            break;
+        case '-':
+            valueSwitchCalc = firstNumber - secondNumber;
+            break;
+        case '*':
+            valueSwitchCalc = firstNumber * secondNumber;
+            break;
+        case '/':
+            valueSwitchCalc = firstNumber / secondNumber;
+            break;
+        case '**':
+            valueSwitchCalc = firstNumber ** secondNumber;
+            break;
+        case '%':
+            valueSwitchCalc = firstNumber % secondNumber;
+            break;
+        default:
+            break;
+    }
+    console.log(`Calculated through the conditional operator 'switch', 'case' and value = ${valueSwitchCalc}`);
+    return valueSwitchCalc;
 }
 
-switch (operation) {
-    case '+':
-        valueSwitchCalc = firstNumber + secondNumber;
-        break;
-    case '-':
-        valueSwitchCalc = firstNumber - secondNumber;
-        break;
-    case '*':
-        valueSwitchCalc = firstNumber * secondNumber;
-        break;
-    case '/':
-        valueSwitchCalc = firstNumber / secondNumber;
-        break;
-    case '**':
-        valueSwitchCalc = firstNumber ** secondNumber;
-        break;
-    case '%':
-        valueSwitchCalc = firstNumber % secondNumber;
-        break;
-    default:
-        break;
+function onErr(err) {
+    console.log(err);
+    return 1;
 }
+const prompt = require('prompt');
 
-console.log(`
-     Calculated through the conditional operator 'if', 'else if' and value = ${valueIfCalc};
-     Calculated through the conditional operator 'switch', 'case' and value = ${valueSwitchCalc};
-`);
+prompt.start();
+
+prompt.get(['firstNumber', 'secondNumber', 'operation'], (err, result) => {
+    if (err) {
+        return onErr(err);
+    }
+    firstNumber = result.firstNumber;
+    secondNumber = result.secondNumber;
+    operation = result.operation;
+    calculateByIfElse();
+    calculateBySwitchCase();
+    return {
+        valueIfCalc,
+        valueSwitchCalc,
+    };
+});
