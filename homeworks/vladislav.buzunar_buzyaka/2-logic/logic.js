5a7a70176c1b74d1521fe239b2276775e48e6048
buzyaka <vladbuzunao@gmail.com>
2022-11-30 03:46:51 +0200

    fixed lints

--- homeworks/vladislav.buzunar_buzyaka/2-logic/logic.js
+++ homeworks/vladislav.buzunar_buzyaka/2-logic/logic.js
@@ -1,62 +1,62 @@
 const firstNumber = 20;
 const secondNumber = 22;
-const operationMult = `*`;
+const operationMult = '*';
 const resultMult = 20 * 22;
-const operationDivis = `/`;
+const operationDivis = '/';
 const resultDivis = 20 / 22;
-const operationAddit = `+`;
+const operationAddit = '+';
 const resultAddit = 20 + 22;
-const operationSubtract = `-`;
+const operationSubtract = '-';
 const resultSubtract = 20 - 22;
 
 // If else statement
-console.log(`If/else statement!!!`);
+console.log('If/else statement!!!');
 // Multiplication
-if (operationMult === `*`) {
+if (operationMult === '*') {
     console.log(`${firstNumber} ${operationMult} ${secondNumber} = ${resultMult}`);
 } else {
-    console.log(`Error...`);
+    console.log('Error...');
 }
 
 // Division
-if (operationDivis === `/`) {
+if (operationDivis === '/') {
     console.log(`${firstNumber} ${operationDivis} ${secondNumber} = ${resultDivis}`);
 } else {
-    console.log(`Error...`);
+    console.log('Error...');
 }
 
 // Addition
-if (operationAddit === `+`) {
+if (operationAddit === '+') {
     console.log(`${firstNumber} ${operationAddit} ${secondNumber} = ${resultAddit}`);
 } else {
-    console.log(`Error...`);
+    console.log('Error...');
 }
 
 // Substraction
-if (operationSubtract === `-`) {
+if (operationSubtract === '-') {
     console.log(`${firstNumber} ${operationSubtract} ${secondNumber} = ${resultSubtract}`);
 } else {
-    console.log(`Error...`);
+    console.log('Error...');
 }
 
 // Switch statement
-const valueOperation = prompt(`Вкажіть оператор який ви би хотіли використувати... Доступні( "*" "/" "+" "-" )`);
+const valueOperation = prompt('Вкажіть оператор який ви би хотіли використувати... Доступні( "*" "/" "+" "-" )');
 
-console.log(`Switch statement!!!`);
+console.log('Switch statement!!!');
 
 switch (valueOperation) {
-    case `*`:
+    case '*':
         console.log(`${firstNumber} ${operationMult} ${secondNumber} = ${resultMult}`);
         break;
-    case `/`:
+    case '/':
         console.log(`${firstNumber} ${operationDivis} ${secondNumber} = ${resultDivis}`);
         break;
-    case `+`:
+    case '+':
         console.log(`${firstNumber} ${operationAddit} ${secondNumber} = ${resultAddit}`);
         break;
-    case `-`:
+    case '-':
         console.log(`${firstNumber} ${operationSubtract} ${secondNumber} = ${resultSubtract}`);
         break;
     default:
-        console.log(`На жаль, такого не існує, введіть з списку доступних...`);
+        console.log('На жаль, такого не існує, введіть з списку доступних...');
 }
