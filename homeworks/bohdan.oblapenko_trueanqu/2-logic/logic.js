d829549837350b80a9be49cccf2956de5c753275
Bohdan Oblapenko <bogdan.oblapenko@gmail.com>
2022-11-30 21:57:56 +0200

    2-logic: add input from console

--- homeworks/bohdan.oblapenko_trueanqu/2-logic/logic.js
+++ homeworks/bohdan.oblapenko_trueanqu/2-logic/logic.js
@@ -1,38 +1,49 @@
-const firstNumber = 20;
-const secondNumber = 22;
-const operation = '+';
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
 
-const notSupportedMessage = `Operation '${operation}' is not yet supported, sorry!`;
+readline.question('Enter first number: ', (firstNumInput) => {
+    readline.question('Enter second number: ', (secondNumInput) => {
+        readline.question('Enter operation: ', (operation) => {
+            const firstNumber = Number(firstNumInput);
+            const secondNumber = Number(secondNumInput);
+            const notSupportedMessage = `Operation '${operation}' is not yet supported, sorry!`;
 
-console.log(`Calculating ${firstNumber} ${operation} ${secondNumber} using if statement:`);
+            console.log(`Calculating ${firstNumber} ${operation} ${secondNumber} using if statement:`);
 
-if (operation === '+') {
-    console.log(firstNumber + secondNumber);
-} else if (operation === '-') {
-    console.log(firstNumber - secondNumber);
-} else if (operation === '*') {
-    console.log(firstNumber * secondNumber);
-} else if (operation === '/') {
-    console.log(firstNumber / secondNumber);
-} else {
-    console.log(notSupportedMessage);
-}
+            if (operation === '+') {
+                console.log(firstNumber + secondNumber);
+            } else if (operation === '-') {
+                console.log(firstNumber - secondNumber);
+            } else if (operation === '*') {
+                console.log(firstNumber * secondNumber);
+            } else if (operation === '/') {
+                console.log(firstNumber / secondNumber);
+            } else {
+                console.log(notSupportedMessage);
+            }
 
-console.log(`Calculating ${firstNumber} ${operation} ${secondNumber} using switch statement:`);
+            console.log(`Calculating ${firstNumber} ${operation} ${secondNumber} using switch statement:`);
 
-switch (operation) {
-    case '+':
-        console.log(firstNumber + secondNumber);
-        break;
-    case '-':
-        console.log(firstNumber - secondNumber);
-        break;
-    case '*':
-        console.log(firstNumber * secondNumber);
-        break;
-    case '/':
-        console.log(firstNumber / secondNumber);
-        break;
-    default:
-        console.log(notSupportedMessage);
-}
+            switch (operation) {
+                case '+':
+                    console.log(firstNumber + secondNumber);
+                    break;
+                case '-':
+                    console.log(firstNumber - secondNumber);
+                    break;
+                case '*':
+                    console.log(firstNumber * secondNumber);
+                    break;
+                case '/':
+                    console.log(firstNumber / secondNumber);
+                    break;
+                default:
+                    console.log(notSupportedMessage);
+            }
+
+            readline.close();
+        });
+    });
+});
