949194846538df5238537d4407b92283dd7e3772
Vladislav.Medvedyev <vlad.medvedyev@gmail.com>
2022-12-02 00:08:40 +0200

    Use require('readline')

--- homeworks/vladyslav.medvediev_VladislavMedvedyev/2-logic/logic.js
+++ homeworks/vladyslav.medvediev_VladislavMedvedyev/2-logic/logic.js
@@ -1,42 +1,53 @@
-const readline = require('readline-sync');
+const readline = require('readline');
 
-const firstNumber = Number(readline.question('Enter the first number '));
-const secondNumber = Number(readline.question('Enter the second number '));
-const operation = readline.question('Enter the operation ');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
 
-function checkEnteredNumbers() {
-    if (Number.isNaN(firstNumber) || Number.isNaN(secondNumber)) {
-        console.log('Please enter correct value of numbers');
-    } else {
-        console.log(`You enter: first number is ${firstNumber}, second number is ${secondNumber}`);
-    }
-}
+rl.question('Enter the first number ', (answer1) => {
+    rl.question('Enter the second number ', (answer2) => {
+        rl.question('Enter the operation ', (answer3) => {
+            const firstNumber = Number(answer1);
+            const secondNumber = Number(answer2);
+            const operation = answer3;
 
-checkEnteredNumbers();
+            function checkEnteredNumbers() {
+                if (Number.isNaN(firstNumber) || Number.isNaN(secondNumber)) {
+                    console.log('Please enter correct value of numbers');
+                } else {
+                    console.log(`You enter: first number is ${firstNumber}, second number is ${secondNumber}`);
+                }
+            }
 
-function calc() {
-    switch (operation) {
-        case '*':
-            console.log(firstNumber * secondNumber);
-            break;
-        case '/':
-            console.log(firstNumber / secondNumber);
-            break;
-        case '+':
-            console.log(firstNumber + secondNumber);
-            break;
-        case '-':
-            console.log(firstNumber - secondNumber);
-            break;
-        case '%':
-            console.log(firstNumber % secondNumber);
-            break;
-        case '**':
-            console.log(firstNumber ** secondNumber);
-            break;
-        default:
-            console.log('Please enter correct value of operation (it can be "+", "-", "*", "/", "%", "**")');
-    }
-}
+            checkEnteredNumbers();
 
-calc();
+            function calc() {
+                switch (operation) {
+                    case '*':
+                        console.log(firstNumber * secondNumber);
+                        break;
+                    case '/':
+                        console.log(firstNumber / secondNumber);
+                        break;
+                    case '+':
+                        console.log(firstNumber + secondNumber);
+                        break;
+                    case '-':
+                        console.log(firstNumber - secondNumber);
+                        break;
+                    case '%':
+                        console.log(firstNumber % secondNumber);
+                        break;
+                    case '**':
+                        console.log(firstNumber ** secondNumber);
+                        break;
+                    default:
+                        console.log('Please enter correct value of operation (it can be "+", "-", "*", "/", "%", "**")');
+                }
+            }
+            calc();
+            rl.close();
+        });
+    });
+});
