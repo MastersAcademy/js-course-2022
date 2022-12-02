ef245804e145fd04027aca82853bc99c07f9d3f9
buzyaka <vladbuzunao@gmail.com>
2022-12-03 00:54:55 +0200

    update the logical part in file switchExample.js

--- homeworks/vladislav.buzunar_buzyaka/2-logic/switchExample.js
+++ homeworks/vladislav.buzunar_buzyaka/2-logic/switchExample.js
@@ -1,7 +1,4 @@
-const operationMult = '*';
-const operationDivis = '/';
-const operationAddit = '+';
-const operationSubtract = '-';
+let result;
 
 // Switch statement
 
@@ -9,18 +6,24 @@ const firstUserNumber = prompt('Вкажіть число х : ');
 const secondUserNumber = prompt('Вкажіть число y : ');
 const valueOperation = prompt('Вкажіть оператор який ви би хотіли використувати між "х" та "y"... Доступні( "*" "/" "+" "-" )');
 
+result = `${firstUserNumber} ${valueOperation} ${secondUserNumber}`;
+
 switch (valueOperation) {
     case '*':
-        alert(`${firstUserNumber} ${operationMult} ${secondUserNumber} = ${firstUserNumber * secondUserNumber}`);
+        result = firstUserNumber * secondUserNumber;
+        alert(`${firstUserNumber} ${valueOperation} ${secondUserNumber} =`, result);
         break;
     case '/':
-        alert(`${firstUserNumber} ${operationDivis} ${secondUserNumber} = ${firstUserNumber / secondUserNumber}`);
+        result = firstUserNumber / secondUserNumber;
+        alert(`${firstUserNumber} ${valueOperation} ${secondUserNumber} =`, result);
         break;
     case '+':
-        alert(`${firstUserNumber} ${operationAddit} ${secondUserNumber} = ${firstUserNumber + secondUserNumber}`);
+        result = firstUserNumber + secondUserNumber;
+        alert(`${firstUserNumber} ${valueOperation} ${secondUserNumber} =`, result);
         break;
     case '-':
-        alert(`${firstUserNumber} ${operationSubtract} ${secondUserNumber} = ${firstUserNumber - secondUserNumber}`);
+        result = firstUserNumber - secondUserNumber;
+        alert(`${firstUserNumber} ${valueOperation} ${secondUserNumber} =`, result);
         break;
     default:
         alert('На жаль, такого не існує, введіть з списку доступних...');
