87022c81ded007c6bda84f4be9c3b7879f877afe
buzyaka <vladbuzunao@gmail.com>
2022-12-02 17:37:00 +0200

    create files switchExample

--- homeworks/vladislav.buzunar_buzyaka/2-logic/logic.js
+++ homeworks/vladislav.buzunar_buzyaka/2-logic/logic.js
@@ -23,26 +23,3 @@ if (operationMult === '*') {
 } else {
     console.log('Error...');
 }
-
-// Switch statement
-
-const firstUserNumber = prompt('Вкажіть число х : ');
-const secondUserNumber = prompt('Вкажіть число y : ');
-const valueOperation = prompt('Вкажіть оператор який ви би хотіли використувати між "х" та "y"... Доступні( "*" "/" "+" "-" )');
-
-switch (valueOperation) {
-    case '*':
-        alert(`${firstUserNumber} ${operationMult} ${secondUserNumber} = ${firstUserNumber * secondUserNumber}`);
-        break;
-    case '/':
-        alert(`${firstUserNumber} ${operationDivis} ${secondUserNumber} = ${firstUserNumber / secondUserNumber}`);
-        break;
-    case '+':
-        alert(`${firstUserNumber} ${operationAddit} ${secondUserNumber} = ${firstUserNumber + secondUserNumber}`);
-        break;
-    case '-':
-        alert(`${firstUserNumber} ${operationSubtract} ${secondUserNumber} = ${firstUserNumber - secondUserNumber}`);
-        break;
-    default:
-        alert('На жаль, такого не існує, введіть з списку доступних...');
-}
---
+++ homeworks/vladislav.buzunar_buzyaka/2-logic/switchExample.js
@@ -0,0 +1,22 @@
+// Switch statement
+
+const firstUserNumber = prompt('Вкажіть число х : ');
+const secondUserNumber = prompt('Вкажіть число y : ');
+const valueOperation = prompt('Вкажіть оператор який ви би хотіли використувати між "х" та "y"... Доступні( "*" "/" "+" "-" )');
+
+switch (valueOperation) {
+    case '*':
+        alert(`${firstUserNumber} ${operationMult} ${secondUserNumber} = ${firstUserNumber * secondUserNumber}`);
+        break;
+    case '/':
+        alert(`${firstUserNumber} ${operationDivis} ${secondUserNumber} = ${firstUserNumber / secondUserNumber}`);
+        break;
+    case '+':
+        alert(`${firstUserNumber} ${operationAddit} ${secondUserNumber} = ${firstUserNumber + secondUserNumber}`);
+        break;
+    case '-':
+        alert(`${firstUserNumber} ${operationSubtract} ${secondUserNumber} = ${firstUserNumber - secondUserNumber}`);
+        break;
+    default:
+        alert('На жаль, такого не існує, введіть з списку доступних...');
+}
