8119546009e45522ba05fd13e7925b0a808650b4
learningtogetherua <learningtogetherua@gmail.com>
2022-12-02 13:31:42 +0200

    with commented readline variant

--- homeworks/ivan.shykhat-sarkisov_learningtogetherua/2-logic/logic-readline.js
+++ homeworks/ivan.shykhat-sarkisov_learningtogetherua/2-logic/logic-readline.js
@@ -2,37 +2,37 @@
 
 // const readlineSync = require('readline-sync');
 
-const usersFirstNumber = readlineSync.questionInt('Input first operand ');
-const usersSecondNumber = readlineSync.questionInt('Input first operand ');
-const usersOperation = readlineSync.question('Input operation ');
+// const usersFirstNumber = readlineSync.questionInt('Input first operand ');
+// const usersSecondNumber = readlineSync.questionInt('Input first operand ');
+// const usersOperation = readlineSync.question('Input operation ');
 
-let resultOfOperation = 0;
+// let resultOfOperation = 0;
 
-switch (usersOperation) {
-    case '*':
-        resultOfOperation = usersFirstNumber * usersSecondNumber;
-        console.log(`Результат обраної операції: ${resultOfOperation}`);
-        break;
-    case '/':
-        resultOfOperation = usersFirstNumber / usersSecondNumber;
-        console.log(`Результат обраної операції: ${resultOfOperation}`);
-        break;
-    case '+':
-        resultOfOperation = usersFirstNumber + usersSecondNumber;
-        console.log(`Результат обраної операції: ${resultOfOperation}`);
-        break;
-    case '-':
-        resultOfOperation = usersFirstNumber - usersSecondNumber;
-        console.log(`Результат обраної операції: ${resultOfOperation}`);
-        break;
-    case '%':
-        resultOfOperation = usersFirstNumber % usersSecondNumber;
-        console.log(`Результат обраної операції: ${resultOfOperation}`);
-        break;
-    case '**':
-        resultOfOperation = usersFirstNumber ** usersSecondNumber;
-        console.log(`Результат обраної операції: ${resultOfOperation}`);
-        break;
-    default:
-        console.log('Обрано відсутній оператор');
-}
+// switch (usersOperation) {
+//     case '*':
+//         resultOfOperation = usersFirstNumber * usersSecondNumber;
+//         console.log(`Результат обраної операції: ${resultOfOperation}`);
+//         break;
+//     case '/':
+//         resultOfOperation = usersFirstNumber / usersSecondNumber;
+//         console.log(`Результат обраної операції: ${resultOfOperation}`);
+//         break;
+//     case '+':
+//         resultOfOperation = usersFirstNumber + usersSecondNumber;
+//         console.log(`Результат обраної операції: ${resultOfOperation}`);
+//         break;
+//     case '-':
+//         resultOfOperation = usersFirstNumber - usersSecondNumber;
+//         console.log(`Результат обраної операції: ${resultOfOperation}`);
+//         break;
+//     case '%':
+//         resultOfOperation = usersFirstNumber % usersSecondNumber;
+//         console.log(`Результат обраної операції: ${resultOfOperation}`);
+//         break;
+//     case '**':
+//         resultOfOperation = usersFirstNumber ** usersSecondNumber;
+//         console.log(`Результат обраної операції: ${resultOfOperation}`);
+//         break;
+//     default:
+//         console.log('Обрано відсутній оператор');
+// }
