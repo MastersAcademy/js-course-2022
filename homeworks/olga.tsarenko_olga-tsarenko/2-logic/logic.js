cbc5c0b26f6f453021eca0ab0770df18f51ea2be
olga-tsarenko <olga.tsarenko7@gmail.com>
2022-12-02 14:45:41 +0200

    add readline question

--- homeworks/olga.tsarenko_olga-tsarenko/2-logic/logic.js
+++ homeworks/olga.tsarenko_olga-tsarenko/2-logic/logic.js
@@ -2,6 +2,7 @@ const firstNumber = 20;
 const secondNumber = 22;
 const operation = '*';
 let result = null;
+let calcResult = null;
 
 function testOperation() {
     if (operation === '+') {
@@ -35,3 +36,36 @@ switch (operation) {
 }
 
 console.log('result (v2):', result);
+
+// add readline
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.question('Please enter the first number : ', (firstNumberCalc) => {
+    rl.question('Please enter the second number : ', (secondNumberCalc) => {
+        rl.question('Please enter the operator : ', (calcOperator) => {
+            switch (calcOperator) {
+                case '+':
+                    calcResult = (+firstNumberCalc) + (+secondNumberCalc);
+                    break;
+                case '-':
+                    calcResult = (+firstNumberCalc) - (+secondNumberCalc);
+                    break;
+                case '*':
+                    calcResult = (+firstNumberCalc) * (+secondNumberCalc);
+                    break;
+                case '/':
+                    calcResult = (+firstNumberCalc) / (+secondNumberCalc);
+                // no default
+            }
+
+            console.log('The result is:', calcResult);
+            rl.close();
+        });
+    });
+});
