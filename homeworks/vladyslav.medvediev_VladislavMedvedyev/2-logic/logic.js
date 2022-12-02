01a79bbb269f83b9c21d873d8cfaa5934ffea6f7
Vladislav.Medvedyev <vlad.medvedyev@gmail.com>
2022-12-02 13:46:19 +0200

    logic outside arrow function

--- homeworks/vladyslav.medvediev_VladislavMedvedyev/2-logic/logic.js
+++ homeworks/vladyslav.medvediev_VladislavMedvedyev/2-logic/logic.js
@@ -9,17 +9,6 @@ let firstNumber;
 let secondNumber;
 let operation;
 
-rl.question('Enter the first number ', (answer1) => {
-    rl.question('Enter the second number ', (answer2) => {
-        rl.question('Enter the operation ', (answer3) => {
-            firstNumber = Number(answer1);
-            secondNumber = Number(answer2);
-            operation = answer3;
-            rl.close();
-        });
-    });
-});
-
 function checkEnteredNumbers() {
     if (Number.isNaN(firstNumber) || Number.isNaN(secondNumber)) {
         console.log('Please enter correct value of numbers');
@@ -27,8 +16,6 @@ function checkEnteredNumbers() {
         console.log(`You enter: first number is ${firstNumber}, second number is ${secondNumber}`);
     }
 }
-
-checkEnteredNumbers();
 
 function calc() {
     switch (operation) {
@@ -54,4 +41,16 @@ function calc() {
             console.log('Please enter correct value of operation (it can be "+", "-", "*", "/", "%", "**")');
     }
 }
-calc();
+
+rl.question('Enter the first number ', (answer1) => {
+    rl.question('Enter the second number ', (answer2) => {
+        rl.question('Enter the operation ', (answer3) => {
+            firstNumber = Number(answer1);
+            secondNumber = Number(answer2);
+            operation = answer3;
+            checkEnteredNumbers();
+            calc();
+            rl.close();
+        });
+    });
+});
