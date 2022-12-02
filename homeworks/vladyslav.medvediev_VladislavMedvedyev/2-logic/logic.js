5f46c2ef2f318f027118d2a212a6735d12d060c0
Vladislav.Medvedyev <vlad.medvedyev@gmail.com>
2022-12-02 13:41:12 +0200

    logic outside arrow function

--- homeworks/vladyslav.medvediev_VladislavMedvedyev/2-logic/logic.js
+++ homeworks/vladyslav.medvediev_VladislavMedvedyev/2-logic/logic.js
@@ -5,49 +5,53 @@ const rl = readline.createInterface({
     output: process.stdout,
 });
 
+let firstNumber;
+let secondNumber;
+let operation;
+
 rl.question('Enter the first number ', (answer1) => {
     rl.question('Enter the second number ', (answer2) => {
         rl.question('Enter the operation ', (answer3) => {
-            const firstNumber = Number(answer1);
-            const secondNumber = Number(answer2);
-            const operation = answer3;
-
-            function checkEnteredNumbers() {
-                if (Number.isNaN(firstNumber) || Number.isNaN(secondNumber)) {
-                    console.log('Please enter correct value of numbers');
-                } else {
-                    console.log(`You enter: first number is ${firstNumber}, second number is ${secondNumber}`);
-                }
-            }
-
-            checkEnteredNumbers();
-
-            function calc() {
-                switch (operation) {
-                    case '*':
-                        console.log(firstNumber * secondNumber);
-                        break;
-                    case '/':
-                        console.log(firstNumber / secondNumber);
-                        break;
-                    case '+':
-                        console.log(firstNumber + secondNumber);
-                        break;
-                    case '-':
-                        console.log(firstNumber - secondNumber);
-                        break;
-                    case '%':
-                        console.log(firstNumber % secondNumber);
-                        break;
-                    case '**':
-                        console.log(firstNumber ** secondNumber);
-                        break;
-                    default:
-                        console.log('Please enter correct value of operation (it can be "+", "-", "*", "/", "%", "**")');
-                }
-            }
-            calc();
+            firstNumber = Number(answer1);
+            secondNumber = Number(answer2);
+            operation = answer3;
             rl.close();
         });
     });
 });
+
+function checkEnteredNumbers() {
+    if (Number.isNaN(firstNumber) || Number.isNaN(secondNumber)) {
+        console.log('Please enter correct value of numbers');
+    } else {
+        console.log(`You enter: first number is ${firstNumber}, second number is ${secondNumber}`);
+    }
+}
+
+checkEnteredNumbers();
+
+function calc() {
+    switch (operation) {
+        case '*':
+            console.log(firstNumber * secondNumber);
+            break;
+        case '/':
+            console.log(firstNumber / secondNumber);
+            break;
+        case '+':
+            console.log(firstNumber + secondNumber);
+            break;
+        case '-':
+            console.log(firstNumber - secondNumber);
+            break;
+        case '%':
+            console.log(firstNumber % secondNumber);
+            break;
+        case '**':
+            console.log(firstNumber ** secondNumber);
+            break;
+        default:
+            console.log('Please enter correct value of operation (it can be "+", "-", "*", "/", "%", "**")');
+    }
+}
+calc();
