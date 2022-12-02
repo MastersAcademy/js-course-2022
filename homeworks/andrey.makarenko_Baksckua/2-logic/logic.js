c9fd48ac95014f9d094c5558824b073cfb581d70
Andrey Makarenko <baksckua@gmail.net>
2022-12-02 18:51:36 +0200

    homeworks-2

--- homeworks/andrey.makarenko_Baksckua/2-logic/logic.js
+++ homeworks/andrey.makarenko_Baksckua/2-logic/logic.js
@@ -3,34 +3,18 @@ const firstNumber = 20;
 const secondNumber = 22;
 const operation = "+";
 
-// if (operation === "+") {
-//     console.log(firstNumber + secondNumber);
-// } else if (operation === "-") {
-//     console.log(firstNumber - secondNumber);
-// } else if (operation === "*") {
-//     console.log(firstNumber * secondNumber);
-// } else if (operation === "/") {
-//     console.log(firstNumber / secondNumber);
-// } else if (operation === "%") {
-//     console.log(firstNumber % secondNumber);
-// } else if (operation === "**") {
-//     console.log(firstNumber ** secondNumber);
-// } else {
-//     console.log(0);
-// }
-
-// // switch
-switch (operation) {
-    case "*":
-        console.log(firstNumber * secondNumber);
-        break;
-    case "+":
-        console.log(firstNumber + secondNumber);
-        break;
-    case "-":
-        console.log(firstNumber - secondNumber);
-        break;
-    case "/":
-        console.log(firstNumber / secondNumber);
-        break;
+if (operation === "+") {
+    console.log(firstNumber + secondNumber);
+} else if (operation === "-") {
+    console.log(firstNumber - secondNumber);
+} else if (operation === "*") {
+    console.log(firstNumber * secondNumber);
+} else if (operation === "/") {
+    console.log(firstNumber / secondNumber);
+} else if (operation === "%") {
+    console.log(firstNumber % secondNumber);
+} else if (operation === "**") {
+    console.log(firstNumber ** secondNumber);
+} else {
+    console.log(0);
 }
