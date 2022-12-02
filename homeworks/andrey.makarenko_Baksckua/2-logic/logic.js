d9b6162ffd9750e4beb00b512c8bf55ffef6870c
Andrey Makarenko <baksckua@gmail.net>
2022-12-02 13:37:43 +0200

    homeworks2

---
+++ homeworks/andrey.makarenko_Baksckua/2-logic/logic.js
@@ -0,0 +1,39 @@
+// homeworks-2
+const firstNumber = 20;
+const secondNumber = 22;
+const operationAddition = "+";
+const operationSubtraction = "-";
+const operationMultiplication = "*";
+const operationDivision = "/";
+const operationRemainderOfDivision = "%";
+const operationElevationToDegrees = "**";
+
+if (operation === "+") {
+    console.log(firstNumber + secondNumber);
+} else if (operation === "-") {
+    console.log(firstNumber - secondNumber);
+} else if (operation === "*") {
+    console.log(firstNumber * secondNumber);
+} else if (operation === "/") {
+    console.log(firstNumber / secondNumber);
+} else if (operation === "%") {
+    console.log(firstNumber % secondNumber);
+} else if (operation === "**") {
+    console.log(firstNumber ** secondNumber);
+}
+
+// switch
+switch (operation) {
+    case "*":
+        console.log(firstNumber * secondNumber);
+        break;
+    case "+":
+        console.log(firstNumber + secondNumber);
+        break;
+    case "-":
+        console.log(firstNumber - secondNumber);
+        break;
+    case "/":
+        console.log(firstNumber / secondNumber);
+        break;
+}
