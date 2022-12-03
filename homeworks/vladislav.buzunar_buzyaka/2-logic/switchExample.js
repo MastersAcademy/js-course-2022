906e2c462d054310b76fe95930b4d5b70361f3c7
buzyaka <vladbuzunao@gmail.com>
2022-12-03 15:06:34 +0200

    upload the logical part in file switchExample.js

--- homeworks/vladislav.buzunar_buzyaka/2-logic/switchExample.js
+++ homeworks/vladislav.buzunar_buzyaka/2-logic/switchExample.js
@@ -2,29 +2,29 @@ let result;
 
 // Switch statement
 
-const firstUserNumber = prompt('Вкажіть число х : ');
-const secondUserNumber = prompt('Вкажіть число y : ');
+const firstUserNumber = Number(prompt('Вкажіть число х : '));
+const secondUserNumber = Number(prompt('Вкажіть число y : '));
 const valueOperation = prompt('Вкажіть оператор який ви би хотіли використувати між "х" та "y"... Доступні( "*" "/" "+" "-" )');
-
-result = `${firstUserNumber} ${valueOperation} ${secondUserNumber}`;
 
 switch (valueOperation) {
     case '*':
         result = firstUserNumber * secondUserNumber;
-        alert(`${firstUserNumber} ${valueOperation} ${secondUserNumber} =`, result);
         break;
     case '/':
         result = firstUserNumber / secondUserNumber;
-        alert(`${firstUserNumber} ${valueOperation} ${secondUserNumber} =`, result);
         break;
     case '+':
         result = firstUserNumber + secondUserNumber;
-        alert(`${firstUserNumber} ${valueOperation} ${secondUserNumber} =`, result);
         break;
     case '-':
         result = firstUserNumber - secondUserNumber;
-        alert(`${firstUserNumber} ${valueOperation} ${secondUserNumber} =`, result);
         break;
     default:
         alert('На жаль, такого не існує, введіть з списку доступних...');
+}
+
+if (result) {
+    alert(`${firstUserNumber} ${valueOperation} ${secondUserNumber} = ${result}`)
+} else {
+    alert('В значення "х" та "у" можна задавати лише числа. Повторіть спробу!');
 }
