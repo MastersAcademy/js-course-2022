59fadaae577234ef804f2827def445f6167b9dfc
KaterinaTkach <kassy1606@gmail.com>
2022-12-03 10:39:52 +0200

    fixed '2-logic' file according to recommendations

--- homeworks/katerina.tkach_KaterinaTkach/2-logic/logic.js
+++ homeworks/katerina.tkach_KaterinaTkach/2-logic/logic.js
@@ -1,48 +1,48 @@
-// const firstNumber = 20;
-// const secondNumber = 22;
-// const operationMultiply = '*';
-// const operationPlus = '+';
-// const operationMinus = '-';
-// const operationDivide = '/';
-//
-// if (operationMultiply  === '*') {
-//     console.log('множення', firstNumber * secondNumber);
-// } else if (operationMultiply  === '+') {
-//     console.log('додавання', firstNumber + secondNumber);
-// }
-//
-// if (operationPlus === '+') {
-//     console.log('додавання', firstNumber + secondNumber);
-// } else if (operationPlus === '-') {
-//     console.log('віднімання', firstNumber - secondNumber);
-// }
-// if (operationMinus === '-') {
-//     console.log('віднімання', firstNumber - secondNumber);
-// } else if (operationPlus === '-') {
-//     console.log('ділення', firstNumber / secondNumber);
-// }
-// if (operationDivide === '/') {
-//     console.log('ділення', firstNumber / secondNumber);
-// } else if (operationDivide === '-') {
-//     console.log('множення', firstNumber * secondNumber);
-// }
-// switch-case
-
 const firstNumber = 20;
 const secondNumber = 22;
+const operationMultiply = '*';
+const operationPlus = '+';
+const operationMinus = '-';
+const operationDivide = '/';
+
+if (operationMultiply === '*') {
+    console.log('множення', firstNumber * secondNumber);
+} else if (operationMultiply === '+') {
+    console.log('додавання', firstNumber + secondNumber);
+}
+
+if (operationPlus === '+') {
+    console.log('додавання', firstNumber + secondNumber);
+} else if (operationPlus === '-') {
+    console.log('віднімання', firstNumber - secondNumber);
+}
+if (operationMinus === '-') {
+    console.log('віднімання', firstNumber - secondNumber);
+} else if (operationPlus === '-') {
+    console.log('ділення', firstNumber / secondNumber);
+}
+if (operationDivide === '/') {
+    console.log('ділення', firstNumber / secondNumber);
+} else if (operationDivide === '-') {
+    console.log('множення', firstNumber * secondNumber);
+}
+// switch-case
+
+const numFirst = 20;
+const numSecond = 22;
 const operation = '/';
-// eslint-disable-next-line default-case
 switch (operation) {
     case '*':
-        console.log('множення', firstNumber * secondNumber);
+        console.log('множення', numFirst * numSecond);
         break;
     case '+':
-        console.log('додавання', firstNumber + secondNumber);
+        console.log('додавання', numFirst + numSecond);
         break;
     case '-':
-        console.log('віднімання', firstNumber - secondNumber);
+        console.log('віднімання', numFirst - numSecond);
         break;
     case '/':
-        console.log('ділення', firstNumber / secondNumber);
+        console.log('ділення', numFirst / numSecond);
         break;
+    // no default
 }
