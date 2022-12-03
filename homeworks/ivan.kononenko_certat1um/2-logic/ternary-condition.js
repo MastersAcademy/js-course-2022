9071e374d7e99f770776d916187f4057d7da09e2
certat1um <canvasworkst@gmail.com>
2022-12-03 21:30:04 +0200

    Updated second homework (2-logic)

--- homeworks/ivan.kononenko_certat1um/2-logic/ternary-condition.js
+++ homeworks/ivan.kononenko_certat1um/2-logic/ternary-condition.js
@@ -2,9 +2,10 @@ const { firstNumber, secondNumber, operation } = require('./variables');
 
 let result = null;
 
-result = (operation === '+') ? firstNumber + secondNumber :
-         (operation === '-') ? firstNumber - secondNumber :
-         (operation === '/') ? firstNumber / secondNumber :
-         (operation === '*') ? firstNumber * secondNumber : null;
+result   = (operation === '+') ? firstNumber + secondNumber
+         : (operation === '-') ? firstNumber - secondNumber
+         : (operation === '/') ? firstNumber / secondNumber
+         : (operation === '*') ? firstNumber * secondNumber
+         : null;
 
 console.log(result);
