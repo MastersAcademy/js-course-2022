627b8ef9aa1e059ba4fcc2925c3c56860862fbf3
K1Koff <111696140+K1Koff@users.noreply.github.com>
2022-12-04 07:37:25 +0200

    fix

--- homeworks/roman.kit_K1Koff/2-logic/logic.js
+++ homeworks/roman.kit_K1Koff/2-logic/logic.js
@@ -69,7 +69,7 @@ const elementStorage = {
 };
 
 function calculateResult() {
-  switch (elementStorage.operator) {
+    switch (elementStorage.operator) {
         case '*': return elementStorage.firstNumber * elementStorage.secondNumber;
         case '/': return elementStorage.firstNumber / elementStorage.secondNumber;
         case '+': return elementStorage.firstNumber + elementStorage.secondNumber;
@@ -77,7 +77,7 @@ function calculateResult() {
         case '**': return elementStorage.firstNumber ** elementStorage.secondNumber;
         case '%': return elementStorage.firstNumber % elementStorage.secondNumber;
         default: return undefined;
-  }
+    }
 }
 
 function validateOperator(operatorSign) {
@@ -93,7 +93,7 @@ function validateNumber(number) {
 function askForInput(partOfTheEquation) {
     return new Promise((resolve) => {
         rl.question(`${partOfTheEquation} `, (answer) => {
-          resolve(answer);
+            resolve(answer);
         });
     });
 }
@@ -102,26 +102,26 @@ async function collectAndSaveData() {
     console.log('Введіть дані для обрахунку:');
     await askForInput('Перше число : ').then((answer) => {
         if (!validateNumber(answer)) {
-          console.error('Неправильні дані!');
-          process.exit();
+            console.error('Неправильні дані!');
+            process.exit();
         }
         elementStorage.firstNumber = +answer;
     });
     await askForInput('Оператор : ').then((answer) => {
         if (!validateOperator(answer)) {
-          console.error('Неправильні дані!');
-          process.exit();
+            console.error('Неправильні дані!');
+            process.exit();
         }
         elementStorage.operator = answer;
     });
     await askForInput('Друге число : ').then((answer) => {
         if (!validateNumber(answer)) {
-          console.error('Неправильні дані!');
-          process.exit();
+            console.error('Неправильні дані!');
+            process.exit();
         }
         elementStorage.secondNumber = +answer;
     });
-  rl.close();
+    rl.close();
 }
 
 async function runCalculator() {
