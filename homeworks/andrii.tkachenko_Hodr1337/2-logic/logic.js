84ec59beed5e7a5fb5e6ac863e88bd4586ffec44
judge-7757 <hodr1337@ukr.net>
2022-12-04 10:30:45 -0500

    Super * task

--- homeworks/andrii.tkachenko_Hodr1337/2-logic/logic.js
+++ homeworks/andrii.tkachenko_Hodr1337/2-logic/logic.js
@@ -1,28 +1,51 @@
-const firstNumber = 20;
-const secondNumber = 22;
-const operation = '*';
-function result(params) {
-    return console.log(params);
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+function calculactionlogic(a, b, opss) {
+    let calculationResult;
+    if (opss === '+') {
+        calculationResult = a + b;
+    } else if (opss === '-') {
+        calculationResult = a - b;
+    } else if (opss === '*') {
+        calculationResult = a * b;
+    } else if (opss === '/') {
+        calculationResult = a / b;
+    } else if (opss === '**') {
+        calculationResult = a ** b;
+    } else if (opss === '%') {
+        calculationResult = a % b;
+    } else {
+        console.log('You entered wrong operator. Try again please');
+    }
+    return calculationResult;
 }
-switch (operation) {
-    case '*':
-        result(firstNumber * secondNumber);
-        break;
-    case '/':
-        result(firstNumber / secondNumber);
-        break;
-    case '+':
-        result(firstNumber + secondNumber);
-        break;
-    case '-':
-        result(firstNumber - secondNumber);
-        break;
-    case '**':
-        result(firstNumber ** secondNumber);
-        break;
-    case '%':
-        result(firstNumber % secondNumber);
-        break;
-    default:
-        result('Something went wrong');
-}
+
+rl.question('Enter first number ', (answer) => {
+    const firstNumber = Number(answer);
+    // eslint-disable-next-line no-restricted-globals
+    if (isNaN(firstNumber)) {
+        console.log('You entered not a number. Try again please');
+        return rl.close();
+    }
+
+    rl.question('Enter operator ', (operator) => {
+        rl.question('Enter second number ', (answer2) => {
+            const secondNumber = Number(answer2);
+            // eslint-disable-next-line no-restricted-globals
+            if (isNaN(secondNumber)) {
+                console.log('You entered not a number. Try again please');
+                return rl.close();
+            }
+            const hereComeTheMagic = calculactionlogic(firstNumber, secondNumber, operator);
+            console.log(hereComeTheMagic);
+            rl.close();
+            return hereComeTheMagic;
+        });
+    });
+    return 'Success';
+});
