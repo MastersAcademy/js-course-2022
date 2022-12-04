bf3f46da43a440f0462bc968ecfc30897ab7d1c7
K1Koff <111696140+K1Koff@users.noreply.github.com>
2022-12-04 07:33:57 +0200

    fix

--- homeworks/roman.kit_K1Koff/2-logic/logic.js
+++ homeworks/roman.kit_K1Koff/2-logic/logic.js
@@ -36,7 +36,7 @@ function switchCaseCalculator() {
         case '**': return operandA ** operandB;
         case '%': return operandA % operandB;
         default: return undefined;
-  }
+    }
 }
 
 console.log(`Відповідь калькулятора на switch / case : ${switchCaseCalculator()}`);
@@ -44,12 +44,12 @@ console.log(`Відповідь калькулятора на switch / case : ${
 /// Просто фанове рішення :
 
 const calculatorFun = {
-  '+': operandA + operandB,
-  '-': operandA - operandB,
-  '*': operandA * operandB,
-  '/': operandA / operandB,
-  '**': operandA ** operandB,
-  '%': operandA % operandB,
+    '+': operandA + operandB,
+    '-': operandA - operandB,
+    '*': operandA * operandB,
+    '/': operandA / operandB,
+    '**': operandA ** operandB,
+    '%': operandA % operandB,
 };
 console.log(`Відповідь фанового рішення : ${calculatorFun[operator]}`);
 
@@ -58,76 +58,76 @@ console.log(`Відповідь фанового рішення : ${calculatorFu
 const readline = require('readline');
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+    input: process.stdin,
+    output: process.stdout,
 });
 
 const elementStorage = {
-  firstNumber: null,
-  secondNumber: null,
-  operator: '+',
+    firstNumber: null,
+    secondNumber: null,
+    operator: '+',
 };
 
 function calculateResult() {
   switch (elementStorage.operator) {
-    case '*': return elementStorage.firstNumber * elementStorage.secondNumber;
-    case '/': return elementStorage.firstNumber / elementStorage.secondNumber;
-    case '+': return elementStorage.firstNumber + elementStorage.secondNumber;
-    case '-': return elementStorage.firstNumber - elementStorage.secondNumber;
-    case '**': return elementStorage.firstNumber ** elementStorage.secondNumber;
-    case '%': return elementStorage.firstNumber % elementStorage.secondNumber;
-    default: return undefined;
+        case '*': return elementStorage.firstNumber * elementStorage.secondNumber;
+        case '/': return elementStorage.firstNumber / elementStorage.secondNumber;
+        case '+': return elementStorage.firstNumber + elementStorage.secondNumber;
+        case '-': return elementStorage.firstNumber - elementStorage.secondNumber;
+        case '**': return elementStorage.firstNumber ** elementStorage.secondNumber;
+        case '%': return elementStorage.firstNumber % elementStorage.secondNumber;
+        default: return undefined;
   }
 }
 
 function validateOperator(operatorSign) {
-  const isOperatorValid = ['+', '-', '*', '/', '%', '**'].includes(operatorSign.trim());
-  return isOperatorValid;
+    const isOperatorValid = ['+', '-', '*', '/', '%', '**'].includes(operatorSign.trim());
+    return isOperatorValid;
 }
 
 function validateNumber(number) {
-  const isNumberValid = Number.isFinite(+(number.trim()));
-  return isNumberValid;
+    const isNumberValid = Number.isFinite(+(number.trim()));
+    return isNumberValid;
 }
 
 function askForInput(partOfTheEquation) {
-  return new Promise((resolve) => {
-    rl.question(`${partOfTheEquation} `, (answer) => {
-      resolve(answer);
+    return new Promise((resolve) => {
+        rl.question(`${partOfTheEquation} `, (answer) => {
+          resolve(answer);
+        });
     });
-  });
 }
 
 async function collectAndSaveData() {
     console.log('Введіть дані для обрахунку:');
-  await askForInput('Перше число : ').then((answer) => {
-    if (!validateNumber(answer)) {
-      console.error('Неправильні дані!');
-      process.exit();
-    }
-    elementStorage.firstNumber = +answer;
-  });
-  await askForInput('Оператор : ').then((answer) => {
-    if (!validateOperator(answer)) {
-      console.error('Неправильні дані!');
-      process.exit();
-    }
-    elementStorage.operator = answer;
-  });
-  await askForInput('Друге число : ').then((answer) => {
-    if (!validateNumber(answer)) {
-      console.error('Неправильні дані!');
-      process.exit();
-    }
-    elementStorage.secondNumber = +answer;
-  });
+    await askForInput('Перше число : ').then((answer) => {
+        if (!validateNumber(answer)) {
+          console.error('Неправильні дані!');
+          process.exit();
+        }
+        elementStorage.firstNumber = +answer;
+    });
+    await askForInput('Оператор : ').then((answer) => {
+        if (!validateOperator(answer)) {
+          console.error('Неправильні дані!');
+          process.exit();
+        }
+        elementStorage.operator = answer;
+    });
+    await askForInput('Друге число : ').then((answer) => {
+        if (!validateNumber(answer)) {
+          console.error('Неправильні дані!');
+          process.exit();
+        }
+        elementStorage.secondNumber = +answer;
+    });
   rl.close();
 }
 
 async function runCalculator() {
-  await collectAndSaveData();
-  const output = calculateResult();
-  console.log(`Відповідь : ${output}`);
+    await collectAndSaveData();
+    const output = calculateResult();
+    console.log(`Відповідь : ${output}`);
 }
 
 runCalculator();
