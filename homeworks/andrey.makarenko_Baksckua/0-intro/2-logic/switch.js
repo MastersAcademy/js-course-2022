50ebd2dbeb5605506d59571fd1ff1b3f1256aa5c
Andrey Makarenko <baksckua@gmail.net>
2022-12-06 10:27:41 +0200

    homework-2

---
+++ homeworks/andrey.makarenko_Baksckua/0-intro/2-logic/switch.js
@@ -0,0 +1,19 @@
+//  switch
+const firstNumber = 8;
+const secondNumber = 6;
+const operation = "+";
+
+switch (operation) {
+    case "*":
+        console.log(firstNumber * secondNumber);
+        break;
+    case "+":
+        console.log(firstNumber + secondNumber);
+        break;
+    case "-":
+        console.log(firstNumber - secondNumber);
+        break;
+    case "/":
+        console.log(firstNumber / secondNumber);
+        break;
+}
