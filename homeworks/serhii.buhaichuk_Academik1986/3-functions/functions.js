bf916fffe3391638853f9ca5e2bb3bea64d9cb86
Academik1986 <servega.ua@gmail.com>
2022-12-08 23:20:48 +0200

    3-Functions

---
+++ homeworks/serhii.buhaichuk_Academik1986/3-functions/functions.js
@@ -0,0 +1,20 @@
+const OIL = 926;
+const WATER = 1000;
+const MERCURY = 1355;
+
+const getWeight = (weight, liquid, count) => {
+    switch (liquid) {
+        case 'OIL':
+        case 'WATER':
+        case 'MERCURY':
+            return weight * count;
+        case '':
+            return 'Please enter liquid';
+    }
+    return `Загальна маса рідини: ${OIL * 3 + WATER * 5 + MERCURY * 2}`;
+};
+
+console.log(getWeight(926, 'OIL', 3)); // Вага олії
+console.log(getWeight(1000, 'WATER', 5)); // Вага води
+console.log(getWeight(1355, 'MERCURY', 2)); // Вага ртуті
+console.log(getWeight()); // Загальна вага
