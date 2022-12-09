a032caf25abe2946862a61747fe4f16532bb7537
learningtogetherua <learningtogetherua@gmail.com>
2022-12-09 13:46:38 +0200

    change linter error (cut of long line)

--- homeworks/ivan.shykhat-sarkisov_learningtogetherua/3-functions/functions.js
+++ homeworks/ivan.shykhat-sarkisov_learningtogetherua/3-functions/functions.js
@@ -1,7 +1,7 @@
 // стартові дані
-const oilThousandLitersWeight = 926;
-const waterThousandLitersWeight = 1000;
-const mercuryThousandLitersWeight = 1355;
+const oil1KLitersWeight = 926;
+const water1KLitersWeight = 1000;
+const mercury1KLitersWeight = 1355;
 
 const oilTankWagonQuantity = 3;
 const waterTankWagonQuantity = 5;
@@ -12,7 +12,7 @@ const waterTankWagonVolume = 1000;
 const mercuryTankWagonVolume = 1000;
 
 // Змінна для першого варіанту обчислення суми трьох рідин
-let summaryLiquidWeight = 0;
+let summaryWeight = 0;
 
 // Ініціалізація функції
 function getWeight(weight, liquid, count) {
@@ -22,16 +22,16 @@ function getWeight(weight, liquid, count) {
 }
 
 // Перший варіант накопичення даних про вагу всіх рідин
-summaryLiquidWeight += getWeight(oilThousandLitersWeight, oilTankWagonVolume, oilTankWagonQuantity);
-summaryLiquidWeight += getWeight(waterThousandLitersWeight, waterTankWagonVolume, waterTankWagonQuantity);
-summaryLiquidWeight += getWeight(mercuryThousandLitersWeight, mercuryTankWagonVolume, mercuryTankWagonQuantity);
+summaryWeight += getWeight(oil1KLitersWeight, oilTankWagonVolume, oilTankWagonQuantity);
+summaryWeight += getWeight(water1KLitersWeight, waterTankWagonVolume, waterTankWagonQuantity);
+summaryWeight += getWeight(mercury1KLitersWeight, mercuryTankWagonVolume, mercuryTankWagonQuantity);
 
 // Другий варіант накопичення даних про вагу всіх рідин [Для коректного виконання необхідно закоментувати вище рядок let summaryLiquidWeight = 0 ]
 // const oilLiquidWeight = getWeight(oilThousandLitersWeight, oilTankWagonVolume, oilTankWagonQuantity);
 // const waterLiquidWeight = getWeight(waterThousandLitersWeight, waterTankWagonVolume, waterTankWagonQuantity);
 // const mercuryLiquidWeight = getWeight(mercuryThousandLitersWeight, mercuryTankWagonVolume, mercuryTankWagonQuantity);
 
-// const summaryLiquidWeight = oilLiquidWeight + waterLiquidWeight + mercuryLiquidWeight;
+// const summaryWeight = oilLiquidWeight + waterLiquidWeight + mercuryLiquidWeight;
 
 // Вивід загальної ваги рідин всіх типів
-console.log(summaryLiquidWeight);
+console.log(summaryWeight);
