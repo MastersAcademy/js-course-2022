c1a9d1ac0495460d1f09a9cd9aab65a52d8ddd7d
RuslanShinoda <korotkoshey@gmail.com>
2022-12-09 20:56:45 +0200

    Homework #3

--- homeworks/ruslan.korotkoshey_RuslanShinoda/3-functions/functions.js
+++ homeworks/ruslan.korotkoshey_RuslanShinoda/3-functions/functions.js
@@ -1,19 +1,13 @@
-const OIL = 926;
-const WATER = 1000;
-const MERCURY = 1355;
-
-const OILquantity = 3;
-const WATERquantity = 5;
-const MERCURYquantity = 2;
-
-function operation(weight, liquid) {
-    return weight * liquid;
-}
-
 function getWeight(weight, liquid, count) {
-    return count(weight, liquid);
+    return weight * count;
 }
 
-const totalWeight = getWeight(OIL, OILquantity, operation)
-+ getWeight(WATER, WATERquantity, operation) + getWeight(MERCURY, MERCURYquantity, operation);
-console.log(totalWeight);
+const waterWeight = getWeight(1000, 'water', 5);
+const oilWeight = getWeight(926, 'oil', 3);
+const mercuryWeight = getWeight(1355, 'mercury', 2);
+
+function totalWeight() {
+    return waterWeight + oilWeight + mercuryWeight;
+}
+const result = totalWeight();
+console.log(result);
