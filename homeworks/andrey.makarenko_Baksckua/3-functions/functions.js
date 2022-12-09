63f6173ccf5cd1a1f1b2027a4d54be2be9ff846c
Andrey Makarenko <baksckua@gmail.net>
2022-12-10 01:39:37 +0200

    homework3

---
+++ homeworks/andrey.makarenko_Baksckua/3-functions/functions.js
@@ -0,0 +1,16 @@
+const OIL = 926;
+const WATER = 1000;
+const MERCURY = 1355;
+
+const numberOfTanksOil = 3;
+const numberOfTanksWeter = 5;
+const numberOfTanksMerkury = 2;
+
+const calcSumOil = OIL * numberOfTanksOil;
+const calcSumWeter = WATER * numberOfTanksWeter;
+const calcSumMerkury = MERCURY * numberOfTanksMerkury;
+
+const getWeight = function () {
+    return calcSumOil + calcSumWete + calcSumMerkury;
+};
+console.log(getWeight);
