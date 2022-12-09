0591cee5d95701599a106061cbb52c4731f8bccc
vladdikhtia <vlad.dikhtiaruk@gmail.com>
2022-12-09 19:54:00 +0100

    functions

--- homeworks/vladyslav.dikhtiaruk_vladdikhtia/3-functions/functions.js
+++ homeworks/vladyslav.dikhtiaruk_vladdikhtia/3-functions/functions.js
@@ -4,13 +4,12 @@ const countOfOil = 3;
 const countOfWater = 5;
 const countOfMercury = 2;
 
-
 const weightOfOIL = 926;
 const weightOfWATER = 1000;
 const weightOfMERCURY = 1355;
 
 function getWeight(weight, liquid, count) {
-    return  weight * liquid * count;
+    return weight * liquid * count;
 }
 
 const weightOfWater = getWeight(weightOfWATER, 1, countOfWater);
@@ -21,11 +20,10 @@ const sumOfWeight = weightOfWater + weightOfOil + weightOfMercury;
 
 console.log('Summary weight is equal to: ', sumOfWeight);
 
-function exp(number, numOfExp) {
+function exp(number, numOfExp) {//task with *
     if (numOfExp === 0) {
         return 1;
-    } else {
-        return number * exp(number, numOfExp - 1);
     }
+        return number * exp(number, numOfExp - 1);
 }
-console.log(exp(2,5));
+console.log('\nResult of exponential function: ', exp(2, 5));
