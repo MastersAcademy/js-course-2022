148193c8f05fdce5b5c70161d34610158ce15ecd
VladLeus <vladyslavleus@gmail.com>
2022-12-09 15:31:45 +0200

    Fixes

--- homeworks/vlad.leus_VladLeus/3-functions/functions.js
+++ homeworks/vlad.leus_VladLeus/3-functions/functions.js
@@ -14,14 +14,12 @@ const sumOfWeights = getWeight(OIL, liquids[0], 3)
 console.log(`The weight of all liquids is equal to ${sumOfWeights}`);
 
 const num = 2;
-const pow = 5;
-
+const pow = 3;
 function getResultOfPower(number, power) {
     if (power === 0) {
         return 1;
-    } else {
-        return number * getResultOfPower(number, power - 1);
     }
+    return number * getResultOfPower(number, power - 1);
 }
 
 const result = getResultOfPower(num, pow);
