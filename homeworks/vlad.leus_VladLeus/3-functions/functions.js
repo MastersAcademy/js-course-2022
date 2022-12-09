3eb2e6333498c207fed94e7c44ded27675cf3d9a
VladLeus <vladyslavleus@gmail.com>
2022-12-09 15:21:01 +0200

    Task 3 - functions.

---
+++ homeworks/vlad.leus_VladLeus/3-functions/functions.js
@@ -0,0 +1,26 @@
+const OIL = 926;
+const WATER = 1000;
+const MERCURY = 1355;
+
+const liquids = ['oil', 'water', 'mercury'];
+
+function getWeight(weight, liquid, count) {
+    return weight * count;
+}
+
+let sumOfWeights = getWeight(OIL, liquids[0], 3) + getWeight(WATER, liquids[1], 5) + getWeight(MERCURY, liquids[2], 2);
+console.log(`The weight of all liquids is equal to ${sumOfWeights}`);
+
+const number = 2;
+const power = 5;
+
+function getResultOfPower(number, power){
+    if (power === 0)
+        return 1;
+    else
+        return number * getResultOfPower(number, --power);
+}
+
+const result = getResultOfPower(number, power);
+console.log(result);
+
