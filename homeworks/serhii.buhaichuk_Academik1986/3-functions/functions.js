e0ff389492d3a755e7f74453998ca1f100c259bd
Academik1986 <servega.ua@gmail.com>
2022-12-10 15:38:31 +0200

    3 functions next next

--- homeworks/serhii.buhaichuk_Academik1986/3-functions/functions.js
+++ homeworks/serhii.buhaichuk_Academik1986/3-functions/functions.js
@@ -6,33 +6,33 @@ const weightOfOilTanker = 926;
 const weightOfWaterTanker = 1000;
 const weightOfMercuryTanker = 1355;
 
-const amountOfOilTanker = 3;
-const amountOfWaterTanker = 5;
-const amountOfMercuryTanker = 2;
+const amountWagonOil = 3;
+const amountWagonWater = 5;
+const amountWagonMercury = 2;
 
-const getWeightOil = (weightOfOilTanker, liquid, amountOfOilTanker) => {
+const getWeightOil = (weightOfOilTanker, liquid, amountWagonOil) => {
     if (liquid === OIL) {
-        return weightOfOilTanker * amountOfOilTanker;
+        return weightOfOilTanker * amountWagonOil;
     }
     return 'Enter liquid';
 };
-console.log(getWeightOil(weightOfOilTanker, OIL, amountOfOilTanker));
+console.log(getWeightOil(weightOfOilTanker, OIL, amountWagonOil));
 
-const getWeightWater = (weightOfWaterTanker, liquid, amountOfWaterTanker) => {
+const getWeightWater = (weightOfWaterTanker, liquid, amountWagonWater) => {
     if (liquid === WATER) {
-        return weightOfWaterTanker * amountOfWaterTanker;
+        return weightOfWaterTanker * amountWagonWater;
     }
     return 'Enter liquid';
 };
-console.log(getWeightWater(weightOfWaterTanker, WATER, amountOfWaterTanker));
+console.log(getWeightWater(weightOfWaterTanker, WATER, amountWagonWater));
 
-const getWeightMercury = (weightOfMercuryTanker, liquid, amountOfMercuryTanker) => {
+const getWeightMercury = (weightOfMercuryTanker, liquid, amountWagonMercury) => {
     if (liquid === MERCURY) {
-        return weightOfMercuryTanker * amountOfMercuryTanker;
+        return weightOfMercuryTanker * amountWagonMercury;
     }
     return 'Enter liquid';
 };
-console.log(getWeightMercury(weightOfMercuryTanker, MERCURY, amountOfMercuryTanker));
+console.log(getWeightMercury(weightOfMercuryTanker, MERCURY, amountWagonMercury));
 
 function getWeightTotalLiquid() {
     return `Загальна маса рідини: ${OIL * 3 + WATER * 5 + MERCURY * 2}`;
