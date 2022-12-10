0bb79436d19b8e71fc7e4a0d08a6216afc5b3ec0
Igor Zapisochnyy <109870514+IgorFromUA@users.noreply.github.com>
2022-12-10 06:58:29 +0200

    extra task, added function calcPOW

--- homeworks/ihor.zapisochnyy_IgorFromUA/3-functions/functions.js
+++ homeworks/ihor.zapisochnyy_IgorFromUA/3-functions/functions.js
@@ -34,3 +34,13 @@ const massMercury = getWeight(WEIGHT_MERCURY, TANK_VOLUME, countTanksMercury);
 const totalMass = massOil + massWater + massMercury;
 
 console.log(`Загальна масса = ${totalMass} кг. \n масса олії = ${massOil} кг. \n маса води = ${massWater} кг. \n маса ртуті = ${massMercury}`);
+
+// --------------Завдання з зірочкою------------------
+const calcPOW = function (num, step) {
+    if (step === 1) {
+        return num;
+    }
+    return num * calcPOW(num, step - 1);
+};
+
+console.log('результат піднесення до степеня', calcPOW(5, 3));
