62edc8331ac58f9e4eb2ed652edd5647a97dbd96
Academik1986 <servega.ua@gmail.com>
2022-12-10 16:08:33 +0200

    3 functions fix

--- homeworks/serhii.buhaichuk_Academik1986/3-functions/functions.js
+++ homeworks/serhii.buhaichuk_Academik1986/3-functions/functions.js
@@ -1,37 +1,34 @@
 const OIL = 926;
-const WATER = 1000;
-const MERCURY = 1355;
-
-const weightOfOilTanker = 926;
-const weightOfWaterTanker = 1000;
-const weightOfMercuryTanker = 1355;
-
-const amountWagonOil = 3;
-const amountWagonWater = 5;
-const amountWagonMercury = 2;
-
 const getWeightOil = (weightOfOilTanker, liquid, amountWagonOil) => {
     if (liquid === OIL) {
         return weightOfOilTanker * amountWagonOil;
     }
     return 'Enter liquid';
 };
+const weightOfOilTanker = 926;
+const amountWagonOil = 3;
 console.log(getWeightOil(weightOfOilTanker, OIL, amountWagonOil));
 
+const WATER = 1000;
 const getWeightWater = (weightOfWaterTanker, liquid, amountWagonWater) => {
     if (liquid === WATER) {
         return weightOfWaterTanker * amountWagonWater;
     }
     return 'Enter liquid';
 };
+const weightOfWaterTanker = 1000;
+const amountWagonWater = 5;
 console.log(getWeightWater(weightOfWaterTanker, WATER, amountWagonWater));
 
+const MERCURY = 1355;
 const getWeightMercury = (weightOfMercuryTanker, liquid, amountWagonMercury) => {
     if (liquid === MERCURY) {
         return weightOfMercuryTanker * amountWagonMercury;
     }
     return 'Enter liquid';
 };
+const weightOfMercuryTanker = 1355;
+const amountWagonMercury = 2;
 console.log(getWeightMercury(weightOfMercuryTanker, MERCURY, amountWagonMercury));
 
 function getWeightTotalLiquid() {
