59a2cb48479c6b05c404fbe13bd69ff9db9847de
Andrey Makarenko <baksckua@gmail.net>
2022-12-11 01:21:11 +0200

    nev

--- homeworks/andrey.makarenko_Baksckua/3-functions/functions1.js
+++ homeworks/andrey.makarenko_Baksckua/3-functions/functions1.js
@@ -1,8 +1,6 @@
 function getIfBasis(basis, exponent) {
-    if (exponent < 1) {
-        return basis;
-    } else {
-        return basis + getIfBasis(basis, exponent - 1);
-    }
+    if (exponent < 1) return basis;
+
+    return basis + getIfBasis(basis, exponent - 1);
 }
 console.log(getIfBasis(2, 8));
