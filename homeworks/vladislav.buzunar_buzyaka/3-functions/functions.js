d1ec6b4f21986e932196e569533666ff6cf13b33
buzyaka <vladbuzunao@gmail.com>
2022-12-10 18:52:23 +0200

    Upload logical part in files functions.js

--- homeworks/vladislav.buzunar_buzyaka/3-functions/functions.js
+++ homeworks/vladislav.buzunar_buzyaka/3-functions/functions.js
@@ -12,9 +12,9 @@ function getWeight(weight, liquid, count) {
     return weight * count;
 }
 
-const totalWeightOil = getWeight(OIL, 0, numOilBank);
-const totalWeightWater = getWeight(WATER, 0, numWaterBank);
-const totalWeightMercury = getWeight(MERCURY, 0, numMercuryBank);
+const totalWeightOil = getWeight(926, OIL, numOilBank);
+const totalWeightWater = getWeight(1000, WATER, numWaterBank);
+const totalWeightMercury = getWeight(1355, MERCURY, numMercuryBank);
 
 console.log(`The total weight of the oil came to the port = ${totalWeightOil}(kg)`);
 console.log(`The total weight of the water came to the port = ${totalWeightWater}(kg)`);
