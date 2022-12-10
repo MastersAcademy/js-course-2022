c97c06a54919d205f9f896d43afb8eaf86b35e69
Vladislav Kyian <vladislavkiyan@ukr.net>
2022-12-10 10:42:45 +0200

    Homework 3 Functions

--- homeworks/vladislav.kyian_vladkiyan23/3-functions/functions.js
+++ homeworks/vladislav.kyian_vladkiyan23/3-functions/functions.js
@@ -28,9 +28,9 @@ function calculateWeight(water, oil, mercury) {
     return totalAmount;
 };
 
-let water = getWeight(1000, WATER, 5);
-let oil = getWeight(926, OIL, 3);
-let mercury = getWeight(1355, MERCURY, 2);
+const water = getWeight(1000, WATER, 5);
+const oil = getWeight(926, OIL, 3);
+const mercury = getWeight(1355, MERCURY, 2);
 
 let weightOfOil;
 let weightOfWater;
