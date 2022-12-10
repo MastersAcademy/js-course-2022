45374352f9a2781a40716d3a316cd3638b5b1634
Vladislav Kyian <vladislavkiyan@ukr.net>
2022-12-10 10:39:16 +0200

    Homework 3 Functions

--- homeworks/vladislav.kyian_vladkiyan23/3-functions/functions.js
+++ homeworks/vladislav.kyian_vladkiyan23/3-functions/functions.js
@@ -2,19 +2,6 @@ const OIL = 926;
 const WATER = 1000;
 const MERCURY = 1355;
 
-let weightOfOil;
-let weightOfWater;
-let weightOfMercury;
-
-let water = getWeight(1000, WATER, 5);
-let oil = getWeight(926, OIL, 3);
-let mercury = getWeight(1355, MERCURY, 2);
-
-function calculateWeight(water, oil, mercury) {
-    let totalAmount = weightOfOil + weightOfWater + weightOfMercury;
-    return totalAmount;
-};
-
 function getWeight(weight, liquid, count) {
     if (liquid === OIL) {
         weightOfOil = OIL * count;
@@ -35,5 +22,18 @@ function getWeight(weight, liquid, count) {
         console.log('Error...');
     }
 };
+
+function calculateWeight(water, oil, mercury) {
+    let totalAmount = weightOfOil + weightOfWater + weightOfMercury;
+    return totalAmount;
+};
+
+let water = getWeight(1000, WATER, 5);
+let oil = getWeight(926, OIL, 3);
+let mercury = getWeight(1355, MERCURY, 2);
+
+let weightOfOil;
+let weightOfWater;
+let weightOfMercury;
 
 console.log(calculateWeight(water, oil, mercury));
