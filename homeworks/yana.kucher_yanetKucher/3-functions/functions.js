4ff74de9a1a578b58ebf66e1bd1d52d95793e2a4
yana-kucher <yana.panasenko@gmail.com>
2022-12-10 23:27:26 +0200

    code refactor

--- homeworks/yana.kucher_yanetKucher/3-functions/functions.js
+++ homeworks/yana.kucher_yanetKucher/3-functions/functions.js
@@ -4,24 +4,34 @@ const MERCURY = 1355;
 const countOil = 3;
 const countWater = 5;
 const countMercury = 2;
-const volumeTank = 1000;
+let totalOilWeight = 0;
+let totalWaterWeight = 0;
+let totalMercuryWeight = 0;
 
 function getWeight(weight, liquid, count) {
-    return (weight * liquid * count);
+    if (liquid === 'OIL') {
+        totalOilWeight = weight * count;
+    } else if (liquid === 'WATER') {
+        totalWaterWeight = weight * count;
+    } else if (liquid === 'MERCURY') {
+        totalMercuryWeight = weight * count;
+    } else {
+        console.log('Error');
+    }
 }
 
-const oilWeight = getWeight(OIL, volumeTank, countOil);
-const waterWeight = getWeight(WATER, volumeTank, countWater);
-const mercuryWeight = getWeight(MERCURY, volumeTank, countMercury);
+getWeight(OIL, 'OIL', countOil);
+getWeight(WATER, 'WATER', countWater);
+getWeight(MERCURY, 'MERCURY', countMercury);
 
-console.log(`Oil Weight: ${oilWeight}`);
-console.log(`Water Weight: ${waterWeight}`);
-console.log(`Mercury Weight: ${mercuryWeight}`);
+console.log(`Oil Weight: ${totalOilWeight}`);
+console.log(`Water Weight: ${totalWaterWeight}`);
+console.log(`Mercury Weight: ${totalMercuryWeight}`);
 
 function getTotalWeight(firstWeight, secondWeight, thirdWeight) {
     return (firstWeight + secondWeight + thirdWeight);
 }
 
-const totalWeight = getTotalWeight(oilWeight, waterWeight, mercuryWeight);
+const totalWeight = getTotalWeight(totalOilWeight, totalWaterWeight, totalMercuryWeight);
 
 console.log(`Total mass of liquids: ${totalWeight}`);
