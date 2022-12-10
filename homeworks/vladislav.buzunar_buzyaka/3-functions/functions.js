5bf3ac43e3076ccacd4791849da8d96c5fbaa006
buzyaka <vladbuzunao@gmail.com>
2022-12-10 18:46:30 +0200

    Change logical part in files functions.js

--- homeworks/vladislav.buzunar_buzyaka/3-functions/functions.js
+++ homeworks/vladislav.buzunar_buzyaka/3-functions/functions.js
@@ -1,3 +1,5 @@
+const TANK_VOLUME = 1000;
+
 const OIL = 926;
 const WATER = 1000;
 const MERCURY = 1355;
@@ -6,21 +8,22 @@ const numOilBank = 3;
 const numWaterBank = 5;
 const numMercuryBank = 2;
 
-function getWeight(weight, amount) {
-    return weight * amount;
+function getWeight(weight, liquid, count) {
+    return weight * count;
 }
 
-const totalWeightOil = getWeight(OIL, numOilBank);
-const totalWeightWater = getWeight(WATER, numWaterBank);
-const totalWeightMercury = getWeight(MERCURY, numMercuryBank);
+const totalWeightOil = getWeight(OIL, 0, numOilBank);
+const totalWeightWater = getWeight(WATER, 0, numWaterBank);
+const totalWeightMercury = getWeight(MERCURY, 0, numMercuryBank);
 
 console.log(`The total weight of the oil came to the port = ${totalWeightOil}(kg)`);
 console.log(`The total weight of the water came to the port = ${totalWeightWater}(kg)`);
 console.log(`The total weight of the mercury came to the port = ${totalWeightMercury}(kg)`);
 
 function totalWeight() {
-    const allWeight = totalWeightOil + totalWeightWater + totalWeightMercury;
-    return allWeight;
+    return totalWeightOil + totalWeightWater + totalWeightMercury;
 }
 
-console.log(`Total weight of liquids that arrived at the port = ${totalWeight()}(kg)`);
+const allWeight = totalWeight();
+
+console.log(`Total weight of liquids that arrived at the port = ${allWeight}(kg)`);
