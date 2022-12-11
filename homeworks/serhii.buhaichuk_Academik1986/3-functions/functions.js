2f2e8358dd94a1749f4911c72215ed9b2466d5fe
Academik1986 <servega.ua@gmail.com>
2022-12-11 17:11:22 +0200

    changes were made taking into account the mentor's comments corrected the names of variables and function operation

--- homeworks/serhii.buhaichuk_Academik1986/3-functions/functions.js
+++ homeworks/serhii.buhaichuk_Academik1986/3-functions/functions.js
@@ -1,29 +1,25 @@
 const totalWeightOneTank = 1000;
-const OIL = 0.926;
-const WATER = 1;
-const MERCURY = 1.355;
+const oilTanker = 0.926;
+const waterTanker = 1;
+const mercuryTanker = 1.355;
 
 const oilCount = 3;
 const waterCount = 5;
 const mercuryCount = 2;
 
-const totaOilWeight = totalWeightOneTank * OIL * oilCount;
-const totaWaterWeight = totalWeightOneTank * WATER * waterCount;
-const totaMercuryWeight = totalWeightOneTank * MERCURY * mercuryCount;
+const totaOilWeight = totalWeightOneTank * oilTanker * oilCount;
+const totaWaterWeight = totalWeightOneTank * waterTanker * waterCount;
+const totaMercuryWeight = totalWeightOneTank * mercuryTanker * mercuryCount;
 const totalLiquid = totaOilWeight + totaWaterWeight + totaMercuryWeight;
-
-function getTotalWeight() {
-    return totalLiquid;
-}
 
 const getWeight = (weight, liquid, count) => {
     if (liquid) {
         return weight * liquid * count;
     }
-    return `Загальна маса рідини: ${getTotalWeight()} кг`;
+    return `Загальна маса рідини: ${totalLiquid}`;
 };
 
-console.log(getWeight(totalWeightOneTank, OIL, oilCount));
-console.log(getWeight(totalWeightOneTank, WATER, waterCount));
-console.log(getWeight(totalWeightOneTank, MERCURY, mercuryCount));
-console.log(getWeight(getTotalWeight()));
+console.log(getWeight(totalWeightOneTank, oilTanker, oilCount));
+console.log(getWeight(totalWeightOneTank, waterTanker, waterCount));
+console.log(getWeight(totalWeightOneTank, mercuryTanker, mercuryCount));
+console.log(getWeight());
