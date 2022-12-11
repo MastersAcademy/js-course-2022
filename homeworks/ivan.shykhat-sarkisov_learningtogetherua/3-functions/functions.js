ef90006bd2b9af9d4c6024355be06c8540f8e1c8
learningtogetherua <learningtogetherua@gmail.com>
2022-12-11 17:11:46 +0200

    change format calculate and return of data

--- homeworks/ivan.shykhat-sarkisov_learningtogetherua/3-functions/functions.js
+++ homeworks/ivan.shykhat-sarkisov_learningtogetherua/3-functions/functions.js
@@ -11,28 +11,18 @@ const oilTankVol = 1000;
 const waterTankVol = 1000;
 const mercuryTankVol = 1000;
 
-// Змінна для першого варіанту обчислення суми трьох рідин
+// Змінна для обчислення суми трьох рідин
 let summaryWeight = 0;
 
 // Ініціалізація функції
 function getWeight(weight, liquid, count) {
-    // Обчислення маси одного типу рідини
-    const oneTypeLiquidWeight = (weight / 1000) * liquid * count;
-    return oneTypeLiquidWeight;
+    // Повернення маси одного типу рідини
+    return (weight / 1000) * liquid * count;
 }
 
-// Перший варіант накопичення даних про вагу всіх рідин
+// Накопичення даних про вагу всіх рідин
 summaryWeight += getWeight(OIL, oilTankVol, oilTankQuantity);
 summaryWeight += getWeight(WATER, waterTankVol, waterTankQuantity);
 summaryWeight += getWeight(MERCURY, mercuryTankVol, mercuryTankQuantity);
 
-// Другий варіант накопичення даних про вагу всіх рідин
-// [Для коректного виконання коментуємо рядок let summaryLiquidWeight = 0 ]
-// const oilLiquidWeight = getWeight(OIL, oilTankVol, oilTankQuantity);
-// const waterLiquidWeight = getWeight(WATER, waterTankVol, waterTankQuantity);
-// const mercuryLiquidWeight=getWeight(MERCURY,mercuryTankVol, mercuryTankQuantity)
-
-// const summaryWeight = oilLiquidWeight + waterLiquidWeight + mercuryLiquidWeight;
-
-// Вивід загальної ваги рідин всіх типів
 console.log(summaryWeight);
