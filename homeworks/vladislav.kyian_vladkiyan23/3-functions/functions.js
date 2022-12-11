df935f9d456783b93c963f376f61d86d83620d65
Vladislav Kyian <vladislavkiyan@ukr.net>
2022-12-11 18:39:41 +0200

    Homework 3 Functions

--- homeworks/vladislav.kyian_vladkiyan23/3-functions/functions.js
+++ homeworks/vladislav.kyian_vladkiyan23/3-functions/functions.js
@@ -10,10 +10,6 @@ const NUMBER_OF_OIL_TANKS = 3;
 const NUMBER_OF_WATER_TANKS = 5;
 const NUMBER_OF_MERCURY_TANKS = 2;
 
-const TOTAL_WEIGHT_OF_OIL = getWeight(OIL, LIQUID_OIL, NUMBER_OF_OIL_TANKS);
-const TOTAL_WEIGHT_OF_WATER = getWeight(WATER, LIQUID_WATER, NUMBER_OF_WATER_TANKS);
-const TOTAL_WEIGHT_OF_MERCURY = getWeight(MERCURY, LIQUID_MERCUTY, NUMBER_OF_MERCURY_TANKS);
-
 function getWeight(weight, liquid, count) {
     const value = weight * count;
     console.log(`Total ${liquid} weight = ${value}`);
@@ -21,7 +17,11 @@ function getWeight(weight, liquid, count) {
 }
 
 function calculateWeight() {
+    const TOTAL_WEIGHT_OF_OIL = getWeight(OIL, LIQUID_OIL, NUMBER_OF_OIL_TANKS);
+    const TOTAL_WEIGHT_OF_WATER = getWeight(WATER, LIQUID_WATER, NUMBER_OF_WATER_TANKS);
+    const TOTAL_WEIGHT_OF_MERCURY = getWeight(MERCURY, LIQUID_MERCUTY, NUMBER_OF_MERCURY_TANKS);
+
     return TOTAL_WEIGHT_OF_OIL + TOTAL_WEIGHT_OF_WATER + TOTAL_WEIGHT_OF_MERCURY;
 }
 
-console.log('total weight of all liquids = ' + calculateWeight(OIL, WATER, MERCURY));
+console.log('total weight of all liquids = ' + calculateWeight());
