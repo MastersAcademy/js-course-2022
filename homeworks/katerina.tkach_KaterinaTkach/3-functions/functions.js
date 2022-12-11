d0850328d710b1358831edc05189442cfe9d71e9
KaterinaTkach <kassy1606@gmail.com>
2022-12-11 09:36:22 +0200

    hw#3

--- homeworks/katerina.tkach_KaterinaTkach/3-functions/functions.js
+++ homeworks/katerina.tkach_KaterinaTkach/3-functions/functions.js
@@ -1,21 +1,43 @@
 const OIL = 926;
 const WATER = 1000;
 const MERCURY = 1355;
-// eslint-disable-next-line consistent-return
-const getWeight = ({ volume, liquid, count }) => {
-    console.log('weig');
-    const coef = (volume / 1000) * count;
-    if (liquid === 'mercury') return coef * MERCURY;
-    if (liquid === 'water') return coef * WATER;
-    if (liquid === 'oil') return coef * OIL;
-};
-// eslint-disable-next-line max-len
-const getAllWeight = (tanks) => tanks.reduce((accumulator, tank) => accumulator + getWeight(tank), 0);
-const tanks = [{ volume: 1000, liquid: 'mercury', count: 2 },
-    { volume: 1000, liquid: 'water', count: 5 },
-    { volume: 1000, liquid: 'oil', count: 3 }];
 
-console.log(getAllWeight(tanks));
+function getWeight({ volume, liquid, count }) {
+    const coefficient = (volume / 1000) * count;
+    if (liquid === 'mercury') return coefficient * MERCURY;
+    if (liquid === 'water') return coefficient * WATER;
+    if (liquid === 'oil') return coefficient * OIL;
+    return true; // <<< this is a bad fix
+}
+const tanks = [
+    { volume: 1000, liquid: 'mercury', count: 2 },
+    { volume: 1000, liquid: 'water', count: 5 },
+    { volume: 1000, liquid: 'oil', count: 3 },
+];
+
+// https://github.com/typescript-eslint/typescript-eslint/issues/2561
+// eslint-disable-next-line no-shadow
+const getAWeight = (tanks) => tanks.reduce((accumulator, tank) => accumulator + getWeight(tank), 0);
+
+console.log('All tanks: ', getAWeight(tanks));
+
+// const getWeight = ({ volume, liquid, count }) => {
+//     console.log('weight');
+//     const coef = (volume / 1000) * count;
+//     if (liquid === 'mercury') return coef * MERCURY;
+//     if (liquid === 'water') return coef * WATER;
+//     if (liquid === 'oil') return coef * OIL;
+// };
+
+// const getAllWeight = (tanks) => tanks
+//     .reduce((accumulator, tank) => accumulator + getWeight(tank), 0);
+// const tanks = [
+//     { volume: 1000, liquid: 'mercury', count: 2 },
+//     { volume: 1000, liquid: 'water', count: 5 },
+//     { volume: 1000, liquid: 'oil', count: 3 }];
+//
+// console.log(getAllWeight(tanks));
+
 // extraTask
 function pow(base, expo) {
     if (expo === 0) { return 1; } return base * pow(base, expo - 1);
