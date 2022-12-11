8daedf44cf40921d5f31e579be7173e70f0ff97a
certat1um <canvasworkst@gmail.com>
2022-12-11 16:40:10 +0200

    Updated third homework (3-functions)

--- homeworks/ivan.kononenko_certat1um/3-functions/functions.js
+++ homeworks/ivan.kononenko_certat1um/3-functions/functions.js
@@ -1,22 +1,22 @@
 const TANK_VOLUME = 1000;
 
 const coefficients = {
-  oil: .926,
-  water: 1,
-  mercury: 1.355,
+    oil: .926,
+    water: 1,
+    mercury: 1.355,
 }
 
 function getWeight(weight, liquid, count) {
-  switch(liquid) {
-    case 'oil':
-      return weight * coefficients.oil * count;
-    case 'water':
-      return weight * coefficients.water * count;
-    case 'mercury':
-      return weight * coefficients.mercury * count;
-    default:
-      throw new Error('No such liquid found');
-  }
+    switch(liquid) {
+        case 'oil':
+        return weight * coefficients.oil * count;
+        case 'water':
+        return weight * coefficients.water * count;
+        case 'mercury':
+        return weight * coefficients.mercury * count;
+        default:
+        throw new Error('No such liquid found');
+    }
 }
 
 const totalWeight = getWeight(TANK_VOLUME, 'oil', 3)
--- homeworks/ivan.kononenko_certat1um/3-functions/pow-function.js
+++ homeworks/ivan.kononenko_certat1um/3-functions/pow-function.js
@@ -2,11 +2,11 @@ const num = 2;
 const degree = 3;
 
 function recursionPow(num, deg) {
-  if(deg > 1) {
-    return num * recursionPow(num, --deg);
-  }
-  
-  return num;
+    if(deg > 1) {
+        return num * recursionPow(num, --deg);
+    }
+    
+    return num;
 }
 
 console.log(recursionPow(num, degree));
