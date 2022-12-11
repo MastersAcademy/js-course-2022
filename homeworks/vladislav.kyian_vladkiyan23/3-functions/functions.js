f63084a810911b5428dfcb87ee346d8e6fa67b00
Vladislav Kyian <vladislavkiyan@ukr.net>
2022-12-11 18:28:31 +0200

    Homeworks 3 Functions

--- homeworks/vladislav.kyian_vladkiyan23/3-functions/functions.js
+++ homeworks/vladislav.kyian_vladkiyan23/3-functions/functions.js
@@ -2,13 +2,22 @@ const OIL = 926;
 const WATER = 1000;
 const MERCURY = 1355;
 
+const NUMBER_OF_OIL_TANKS = 3;
+const NUMBER_OF_WATER_TANKS = 5;
+const NUMBER_OF_MERCURY_TANKS = 2;
+
+const LIQUID_OIL = 'oil';
+const LIQUID_WATER = 'water';
+const LIQUID_MERCUTY = 'mercury';
+
 function getWeight(weight, liquid, count) {
     const value = weight * count;
+    console.log(`Total ${liquid} weight = ${value}`);
     return value;
 }
 
 function calculateWeight() {
-    return getWeight(926, OIL, 3) + getWeight(1000, WATER, 5) + getWeight(1355, MERCURY, 2);
+    return getWeight(OIL, LIQUID_OIL, NUMBER_OF_OIL_TANKS) + getWeight(WATER, LIQUID_WATER, NUMBER_OF_WATER_TANKS) + getWeight(MERCURY, LIQUID_MERCUTY, NUMBER_OF_MERCURY_TANKS);
 }
 
-console.log(calculateWeight(OIL, WATER, MERCURY));
+console.log("total weight of all liquids = " + calculateWeight(OIL, WATER, MERCURY));
