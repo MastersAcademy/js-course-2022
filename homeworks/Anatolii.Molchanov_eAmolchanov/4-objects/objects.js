e85480c4fdf2e5125a5dbe14578bffcff3abdc31
Anatolii Molchanov <e.anatolii.molchanov@gmail.com>
2022-12-18 22:00:45 +0200

    fix comments

--- homeworks/Anatolii.Molchanov_eAmolchanov/4-objects/objects.js
+++ homeworks/Anatolii.Molchanov_eAmolchanov/4-objects/objects.js
@@ -2,11 +2,12 @@ const tamagochi = {
     health: 5,
     happiness: 5,
     satiety: 3,
-    helthUp() {
+    healthUp() {
         this.health++;
     },
     helthDown() {
         this.health--;
+        this.happinessDown();
     },
     happinessUp() {
         this.happiness++;
@@ -20,17 +21,29 @@ const tamagochi = {
     satietyDown() {
         this.satiety--;
     },
+    eat() {
+        this.satietyUp();
+        this.healthUp();
+    },
     play() {
         this.helthDown();
         this.satietyDown();
-        this.happinessDown();
+        this.happinessUp();
 
         if (!this.health || !this.happiness || !this.satiety) {
             console.log('Game over');
+        } else if (this.satiety === 1) {
+            console.log('I`m hungry');
+        } else {
+            console.log('Wow, that was cool!');
         }
     },
 };
 
 tamagochi.play();
+tamagochi.play();
+tamagochi.eat();
+tamagochi.eat();
+tamagochi.eat();
 tamagochi.play();
 tamagochi.play();
