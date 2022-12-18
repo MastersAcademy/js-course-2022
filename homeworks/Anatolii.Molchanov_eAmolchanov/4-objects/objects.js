a1f6879e9c49dc0e751aca93b6534bd04b8f2809
Anatolii Molchanov <e.anatolii.molchanov@gmail.com>
2022-12-18 12:29:04 +0200

    missed spaces

--- homeworks/Anatolii.Molchanov_eAmolchanov/4-objects/objects.js
+++ homeworks/Anatolii.Molchanov_eAmolchanov/4-objects/objects.js
@@ -1,34 +1,34 @@
 const tamagochi = {
-  health: 5,
-  happiness: 5,
-  satiety: 3,
-  helthUp() {
-    this.health++;
-  },
-  helthDown() {
-    this.health--;
-  },
-  happinessUp() {
-    this.happiness++;
-  },
-  happinessDown() {
-    this.happiness--;
-  },
-  satietyUp() {
-    this.satiety++;
-  },
-  satietyDown() {
-    this.satiety--;
-  },
-  play() {
-    this.helthDown();
-    this.satietyDown();
-    this.happinessDown();
+    health: 5,
+    happiness: 5,
+    satiety: 3,
+    helthUp() {
+        this.health++;
+    },
+    helthDown() {
+        this.health--;
+    },
+    happinessUp() {
+        this.happiness++;
+    },
+    happinessDown() {
+        this.happiness--;
+    },
+    satietyUp() {
+        this.satiety++;
+    },
+    satietyDown() {
+        this.satiety--;
+    },
+    play() {
+        this.helthDown();
+        this.satietyDown();
+        this.happinessDown();
 
-    if (!this.health || !this.happiness || !this.satiety) {
-      console.log('Game over');
-    }
-  },
+        if (!this.health || !this.happiness || !this.satiety) {
+            console.log("Game over");
+        }
+    },
 };
 
 tamagochi.play();
