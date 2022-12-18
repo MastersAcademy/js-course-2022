d1b47ab9903ac50e28117555c2dd94fcb1e7b91a
Academik1986 <servega.ua@gmail.com>
2022-12-18 16:33:15 +0200

    Created Tamagochi and corrected checkIsAlive

--- homeworks/serhii.buhaichuk_Academik1986/4-objects/objects.js
+++ homeworks/serhii.buhaichuk_Academik1986/4-objects/objects.js
@@ -2,34 +2,32 @@ const tamagochi = {
     health: 50,
     happy: 50,
     satiety: 50,
-    showTamagochiIndicators() {
-        console.log(`Hi! My name is tamagochi\u{1F60D}:${this.name}! Here is my scores`);
+
+    play() {
+        tamagochi.health -= 1;
+        tamagochi.happy += 1;
+        tamagochi.satiety -= 1;
+        console.log(`Tamagochi health: ${tamagochi.health}`);
+        console.log(`Tamagochi happy: ${tamagochi.happy}`);
+        console.log(`Tamagochi satiety: ${tamagochi.satiety}`);
+    },
+
+    feed() {
+        tamagochi.health += 1;
+        tamagochi.happy -= 1;
+        tamagochi.satiety += 1;
+        console.log(`Tamagochi health: ${tamagochi.health}`);
+        console.log(`Tamagochi happy: ${tamagochi.happy}`);
+        console.log(`Tamagochi satiety: ${tamagochi.satiety}`);
+    },
+    checkIsAlive() {
+        if (tamagochi.health <= 0 || tamagochi.satiety <= 0 || tamagochi.happy <= 0) {
+            return console.log('GAME OVER:Tamagochi died');
+        }
+        return console.log('Tamagochi is alive');
     },
 };
 
-const tamagochi1 = Object.create(tamagochi);
-tamagochi1.name = 'Yamakasy';
-tamagochi1.health = 20;
-tamagochi1.happy = 20;
-tamagochi1.satiety = 20;
-tamagochi1['favorite eat'] = 'cookies';
-tamagochi1.showTamagochiIndicators();
-console.log(tamagochi1);
-
-const tamagochi2 = Object.create(tamagochi);
-tamagochi2.name = 'Kitana';
-tamagochi2.health = 37;
-tamagochi2.happy = 42;
-tamagochi2.satiety = 85;
-tamagochi2['favorite drink'] = 'pineapple juice';
-tamagochi2.showTamagochiIndicators();
-console.log(tamagochi2);
-
-const tamagochi3 = Object.create(tamagochi);
-tamagochi3.name = 'Yakudza';
-tamagochi3.health = 67;
-tamagochi3.happy = 88;
-tamagochi3.satiety = 77;
-tamagochi3['favorite weapon'] = 'knife';
-tamagochi3.showTamagochiIndicators();
-console.log(tamagochi3);
+tamagochi.play();
+tamagochi.feed();
+tamagochi.checkIsAlive();
