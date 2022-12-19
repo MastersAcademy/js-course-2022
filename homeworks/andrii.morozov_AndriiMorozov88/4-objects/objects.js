4d071ccb9f488d92fb18a5c8aaf6c7c447602f26
Andrii Morozov <slon88888@ukr.net>
2022-12-20 00:13:07 +0200

    delete styles, left only one tamagochi

--- homeworks/andrii.morozov_AndriiMorozov88/4-objects/objects.js
+++ homeworks/andrii.morozov_AndriiMorozov88/4-objects/objects.js
@@ -1,82 +1,46 @@
 const pet = document.querySelector('[data-pet]');
-const inputContainer = document.querySelector('[data-input-container]');
+const main = document.querySelector('[data-main]');
 const playButtonEl = document.querySelector('[data-play]');
 const foodButtonEl = document.querySelector('[data-food]');
-const createPetButtonEl = document.querySelector('[data-create-pet]');
-const inputPetName = document.querySelector('[data-input-pet-name]');
-const inputHealth = document.querySelector('[data-input-health]');
-const inputHappiness = document.querySelector('[data-input-happiness]');
-const inputSatiety = document.querySelector('[data-input-satiety]');
-const submitButtonEl = document.querySelector('[data-submit]');
-const createdPets = document.querySelector('[data-created-pets]');
-const petArray = [];
-let template = {
+const template = {
     name: 'templatePet',
     health: 5,
     happiness: 5,
     satiety: 5,
 };
-petArray.push(template);
+const picachu = Object.create(template);
+picachu.name = 'Picachu';
+picachu.health = 10;
+picachu.happiness = 10;
+picachu.satiety = 10;
 function showTemplate() {
-    const keysArray = Object.keys(template);
+    const keysArray = Object.keys(picachu);
     pet.innerHTML = '';
     keysArray.forEach((key) => {
         const objectField = document.createElement('p');
         pet.append(objectField);
-        objectField.innerText = `${key} : ${template[key]}`;
+        objectField.innerText = `${key} : ${picachu[key]}`;
     });
 }
-function showCreatedPets() {
-    createdPets.innerText = '';
-    petArray.forEach((element) => {
-        const createdPet = document.createElement('button');
-        createdPets.append(createdPet);
-        createdPet.classList.add('created-pets');
-        createdPet.innerText = element.name;
-        createdPet.addEventListener('click', () => {
-            template = element;
-            showTemplate();
-        });
-    });
-}
-showCreatedPets();
 function changeFoodState() {
-    template.happiness++;
-    template.health++;
-    template.satiety++;
+    picachu.happiness++;
+    picachu.health++;
+    picachu.satiety++;
     showTemplate();
 }
 function changePlayState() {
-    template.happiness += 2;
-    template.health++;
-    template.satiety--;
-    if (template.happiness * template.health * template.satiety) {
+    picachu.happiness += 2;
+    picachu.health++;
+    picachu.satiety--;
+    if (picachu.happiness * picachu.health * picachu.satiety) {
         showTemplate();
     } else {
         showTemplate();
+        main.innerText = 'Game Over';
         playButtonEl.removeEventListener('click', changePlayState);
         foodButtonEl.removeEventListener('click', changeFoodState);
     }
 }
-function createPet() {
-    inputContainer.classList.toggle('hidden');
-    createPetButtonEl.classList.toggle('disabled');
-    submitButtonEl.addEventListener('click', () => {
-        if (inputPetName.value.length > 0) {
-            const newObject = Object.create(template);
-            petArray.push(newObject);
-            newObject.name = inputPetName.value;
-            newObject.health = Number(inputHealth.value);
-            newObject.happiness = Number(inputHappiness.value);
-            newObject.satiety = Number(inputSatiety.value);
-            showCreatedPets();
-            inputContainer.reset();
-            inputContainer.classList.toggle('hidden');
-            createPetButtonEl.classList.toggle('disabled');
-        }
-    });
-}
 showTemplate();
 playButtonEl.addEventListener('click', changePlayState);
 foodButtonEl.addEventListener('click', changeFoodState);
-createPetButtonEl.addEventListener('click', createPet);
