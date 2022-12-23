79ea737f609030609edd5e552f1f0a67f693421b
Andrii Morozov <slon88888@ukr.net>
2022-12-23 13:43:28 +0200

    added second task

--- homeworks/andrii.morozov_AndriiMorozov88/5-arrays/arrays.js
+++ homeworks/andrii.morozov_AndriiMorozov88/5-arrays/arrays.js
@@ -1,4 +1,4 @@
-const lowRowStarsAmount = 75;
+const lowRowStarsAmount = 21;
 function pine(height) {
     let starAmount = height % 2 === 1 ? 1 : 2;
     const pineHeight = Math.round(height / 2);
@@ -19,3 +19,21 @@ function pine(height) {
     }
 }
 pine(lowRowStarsAmount);
+
+const arr = [
+    [1, 4, 2, 4, 4, 2, 4, 3, 2, 6, 2, 4, 2, 4, 3, 1, 2, 4, 3],
+    [1, 1, 3, 1, 1, 3, 3, 1, 6, 6, 8, 3, 3, 3, 3, 1, 4, 1, 2],
+    [1, 2, 1, 4, 1, 1, 4, 7, 8, 8, 7, 7, 1, 1, 4, 3, 1, 1, 2],
+    [3, 2, 3, 3, 4, 2, 7, 8, 6, 8, 6, 7, 8, 1, 2, 3, 3, 3, 4],
+    [4, 1, 3, 3, 2, 8, 7, 8, 6, 7, 6, 7, 6, 7, 2, 4, 3, 2, 2],
+    [3, 1, 2, 1, 7, 7, 8, 8, 8, 7, 6, 8, 8, 6, 8, 4, 1, 3, 2],
+    [2, 1, 1, 7, 6, 7, 7, 8, 7, 6, 8, 6, 6, 6, 6, 8, 4, 4, 3],
+    [4, 1, 6, 7, 8, 7, 8, 8, 6, 6, 6, 7, 6, 6, 6, 7, 6, 1, 2],
+    [1, 6, 6, 6, 6, 8, 8, 7, 6, 6, 7, 8, 7, 8, 8, 7, 6, 7, 1],
+    [8, 6, 8, 6, 6, 7, 7, 6, 6, 7, 8, 7, 6, 6, 6, 6, 7, 8, 7],
+];
+const transformArr = arr.reduce((acc, value) => acc.concat(value));
+transformArr.forEach((element, index) => {
+    transformArr[index] = element > 5 ? '*' : ' ';
+});
+console.log(transformArr.join(''));
