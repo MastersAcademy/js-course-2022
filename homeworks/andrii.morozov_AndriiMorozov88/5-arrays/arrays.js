7092bfd73645cf478fd1aedfba6e91211b8952c6
Andrii Morozov <slon88888@ukr.net>
2022-12-23 12:36:27 +0200

    added first task with *

--- homeworks/andrii.morozov_AndriiMorozov88/5-arrays/arrays.js
+++ homeworks/andrii.morozov_AndriiMorozov88/5-arrays/arrays.js
@@ -1,14 +1,21 @@
-const lowRowStarsAmount = 10;
-let upRowAmount = lowRowStarsAmount % 2 === 1 ? 1 : 2;
-const pineHeight = Math.round(lowRowStarsAmount / 2);
-for (let count = 0; count < pineHeight; count++) {
-    let dashAmount = lowRowStarsAmount - upRowAmount;
-    const starsArray = new Array(upRowAmount);
-    const dashArray = new Array(dashAmount / 2);
-    starsArray.fill('*');
-    dashArray.fill('-');
-    const pineArray = dashArray.concat(starsArray, dashArray);
-    upRowAmount += 2;
-    dashAmount -= 2;
-    console.log(pineArray.join(''));
+const lowRowStarsAmount = 75;
+function pine(height) {
+    let starAmount = height % 2 === 1 ? 1 : 2;
+    const pineHeight = Math.round(height / 2);
+    for (let count = 0; count < pineHeight; count++) {
+        let dashAmount = lowRowStarsAmount - starAmount;
+        const starsArray = new Array(starAmount);
+        const dashArray = new Array(dashAmount / 2);
+        starsArray.fill('*');
+        dashArray.fill('-');
+        if (count % 2 === 1) {
+            starsArray[0] = '@';
+            starsArray[starsArray.length - 1] = '@';
+        }
+        const pineArray = dashArray.concat(starsArray, dashArray);
+        starAmount += 2;
+        dashAmount -= 2;
+        console.log(pineArray.join(''));
+    }
 }
+pine(lowRowStarsAmount);
