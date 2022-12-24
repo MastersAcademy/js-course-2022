383c4d9aa3fb3e5d488c7e5e586214177bb2d840
VladLeus <vladyslavleus@gmail.com>
2022-12-24 11:40:23 +0200

    Lint fixes.

--- homeworks/vlad.leus_VladLeus/5-arrays/arrays.js
+++ homeworks/vlad.leus_VladLeus/5-arrays/arrays.js
@@ -1,8 +1,8 @@
 /* Task 1 - Christmas Tree */
 
 function makeTree(height) {
-    let tree = [];
-    let width = height * 2 - 1;
+    const tree = [];
+    const width = height * 2 - 1;
     let treeTemp = [width];
     const midIndex = parseInt(((width) / 2), 10);
     let rightIndex = midIndex;
@@ -38,17 +38,17 @@ function drawTree(tree) {
     }
 }
 
-let treeHeight = 15;
-let christmasTree = makeTree(treeHeight);
+const treeHeight = 15;
+const christmasTree = makeTree(treeHeight);
 drawTree(christmasTree);
 
 /* Task 2 - array to string array */
 
 function arrayToStringArray(array) {
     let str = '';
-    let result = [];
+    const result = [];
     for (let i = 0; i < array.length; i++) {
-        let tempArray = array[i];
+        const tempArray = array[i];
         for (let j = 0; j < tempArray.length; j++) {
             if (tempArray[j] < 5) {
                 tempArray[j] = ' ';
@@ -64,7 +64,7 @@ function arrayToStringArray(array) {
     return result;
 }
 
-let myArray = [
+const myArray = [
     [1, 4, 2, 4, 4, 2, 4, 3, 2, 6, 2, 4, 2, 4, 3, 1, 2, 4, 3],
     [1, 1, 3, 1, 1, 3, 3, 1, 6, 6, 8, 3, 3, 3, 3, 1, 4, 1, 2],
     [1, 2, 1, 4, 1, 1, 4, 7, 8, 8, 7, 7, 1, 1, 4, 3, 1, 1, 2],
@@ -76,16 +76,17 @@ let myArray = [
     [1, 6, 6, 6, 6, 8, 8, 7, 6, 6, 7, 8, 7, 8, 8, 7, 6, 7, 1],
     [8, 6, 8, 6, 6, 7, 7, 6, 6, 7, 8, 7, 6, 6, 6, 6, 7, 8, 7],
 ];
-/* let result = arrayToStringArray(myArray);
+const result = arrayToStringArray(myArray);
+console.log();
 for (let i = 0; i < result.length; i++) {
     console.log(result[i]);
-} */
+}
 
 /* Task 3 - Pretty Christmas Tree */
 
 function makeFancyTree(height) {
-    let fancyTree = [];
-    let width = height * 2 - 1;
+    const fancyTree = [];
+    const width = height * 2 - 1;
     let treeTemp = [width];
     const midIndex = parseInt(((width) / 2), 10);
     let rightIndex = midIndex;
@@ -113,6 +114,7 @@ function makeFancyTree(height) {
     return fancyTree;
 }
 
-/* let prettyTreeHeight = 15;
+let prettyTreeHeight = 15;
 let prettyChristmasTree = makeFancyTree(prettyTreeHeight);
-drawTree(prettyChristmasTree); */
+console.log();
+drawTree(prettyChristmasTree);
