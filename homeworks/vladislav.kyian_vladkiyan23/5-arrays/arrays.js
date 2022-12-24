277b0edfe5da4ddc8a215da52c4f5d6d5e319862
Vladislav Kyian <vladislavkiyan@ukr.net>
2022-12-24 17:27:51 +0200

    Hommework 5 Arrays

--- homeworks/vladislav.kyian_vladkiyan23/5-arrays/arrays.js
+++ homeworks/vladislav.kyian_vladkiyan23/5-arrays/arrays.js
@@ -18,10 +18,10 @@ function replaceArrayItems() {
     array.forEach((item) => {
         replaceArray.push(item.map((value) => {
             if (value < 5) {
-                return ' ';
+                return value = ' ';
             }
             if (value > 5) {
-                return '*';
+                return value = '*';
             }
         }));
     });
