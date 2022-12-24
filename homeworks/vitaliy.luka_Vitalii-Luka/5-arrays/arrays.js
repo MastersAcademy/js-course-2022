992a73c492ae22bc3d5a0bb3be4a00b1fc216a48
Vitaliy Luka <luka.vitalii.ck@gmail.com>
2022-12-24 17:28:18 +0200

    5-arrays: added task 2

--- homeworks/vitaliy.luka_Vitalii-Luka/5-arrays/arrays.js
+++ homeworks/vitaliy.luka_Vitalii-Luka/5-arrays/arrays.js
@@ -2,8 +2,6 @@
 Homework 5 - Arrays
 */
 
-// Part.1
-console.log('-----  Part.1 -----\n');
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -11,6 +9,7 @@ const rl = readline.createInterface({
     output: process.stdout,
 });
 
+// Part.1
 function createTree(height) {
     for (let i = 0; i < height; i++) {
         console.log(`${' '.repeat(height - i)}*${'*'.repeat(i * 2)}`);
@@ -19,6 +18,40 @@ function createTree(height) {
 
 function setinput(input) {
     createTree(input);
-    rl.close();
 }
-rl.question('Enter the height of the Christmas tree: ', setinput);
+
+const askHeight = () => new Promise((resolve) => {
+    rl.question('Enter the height of the Christmas tree: ', (answer) => {
+        setinput(answer);
+        resolve();
+    });
+});
+
+// Part.2
+const mainArr = [
+    [1, 4, 2, 4, 4, 2, 4, 3, 2, 6, 2, 4, 2, 4, 3, 1, 2, 4, 3],
+    [1, 1, 3, 1, 1, 3, 3, 1, 6, 6, 8, 3, 3, 3, 3, 1, 4, 1, 2],
+    [1, 2, 1, 4, 1, 1, 4, 7, 8, 8, 7, 7, 1, 1, 4, 3, 1, 1, 2],
+    [3, 2, 3, 3, 4, 2, 7, 8, 6, 8, 6, 7, 8, 1, 2, 3, 3, 3, 4],
+    [4, 1, 3, 3, 2, 8, 7, 8, 6, 7, 6, 7, 6, 7, 2, 4, 3, 2, 2],
+    [3, 1, 2, 1, 7, 7, 8, 8, 8, 7, 6, 8, 8, 6, 8, 4, 1, 3, 2],
+    [2, 1, 1, 7, 6, 7, 7, 8, 7, 6, 8, 6, 6, 6, 6, 8, 4, 4, 3],
+    [4, 1, 6, 7, 8, 7, 8, 8, 6, 6, 7, 7, 6, 6, 6, 7, 6, 1, 2],
+    [1, 6, 6, 6, 6, 8, 8, 7, 6, 6, 7, 8, 7, 8, 8, 7, 6, 7, 1],
+    [8, 6, 8, 6, 6, 7, 7, 6, 6, 7, 8, 7, 6, 6, 6, 6, 7, 8, 7],
+];
+
+const christmasTree = mainArr.map((arrTransform) => arrTransform.reduce((number, currentValue) => {
+    const value = (currentValue > 5) ? '*' : ' ';
+    return number + value;
+}, ''));
+
+const runHomeTask = async () => {
+    console.log('-----  Part.1 -----\n');
+    await askHeight();
+    rl.close();
+    console.log('\n-----  Part.2 -----\n');
+    console.log(christmasTree);
+};
+
+runHomeTask();
