26118210b1e6b86953b8dc9e49e33ee3b295105f
olgadobrodii <dobrodey.olga@gmail.com>
2022-12-27 00:40:33 +0200

    added homework4 4-objects

---
+++ homeworks/olha.dobrodii_olgadobrodii/4-objects/objects.js
@@ -0,0 +1,43 @@
+const tamagochi = {
+    maxHealth: 10,
+    maxHappiness: 10,
+    maxBellyful: 10,
+    growUp() {
+      this.health += 1;
+      this.happiness += 1;
+      this.bellyfull += 1;
+    },
+    reduction() {
+      this.health -= 1;
+      this.happiness -= 1;
+      this.bellyfull -= 1;
+    },
+    playWithMe() {
+      this.health -= 2;
+      this.happiness += 1;
+      this.bellyfull -= 3;
+    },
+    feedMe() {
+      this.health += 2;
+      this.happiness += 1;
+      this.bellyfull += 3;
+    }
+};
+tamagochi.checkLife = function() {
+  if ((this.health) <= 0
+    || (this.happiness) <= 0
+    || (this.bellyful) <= 0) {
+      return false;
+
+    }
+    else {
+      return true
+    }
+}  
+tamagochi.showMassage = function () {
+  if (this.checkLife() === false) {
+    return 'happy pet'
+  } else {
+    return 'sorry, you pet died'
+  }
+}
