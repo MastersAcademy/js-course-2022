76f5fcbc4fe61b0b9219c52240a1872e38931ab8
olgadobrodii <dobrodey.olga@gmail.com>
2022-12-27 00:59:22 +0200

    added fix homework4 4-objects

--- homeworks/olha.dobrodii_olgadobrodii/4-objects/objects.js
+++ homeworks/olha.dobrodii_olgadobrodii/4-objects/objects.js
@@ -3,41 +3,40 @@ const tamagochi = {
     maxHappiness: 10,
     maxBellyful: 10,
     growUp() {
-      this.health += 1;
-      this.happiness += 1;
-      this.bellyfull += 1;
+        this.health += 1;
+        this.happiness += 1;
+        this.bellyfull += 1;
     },
     reduction() {
-      this.health -= 1;
-      this.happiness -= 1;
-      this.bellyfull -= 1;
+        this.health -= 1;
+        this.happiness -= 1;
+        this.bellyfull -= 1;
     },
     playWithMe() {
-      this.health -= 2;
-      this.happiness += 1;
-      this.bellyfull -= 3;
+        this.health -= 2;
+        this.happiness += 1;
+        this.bellyfull -= 3;
     },
     feedMe() {
-      this.health += 2;
-      this.happiness += 1;
-      this.bellyfull += 3;
-    }
+        this.health += 2;
+        this.happiness += 1;
+        this.bellyfull += 3;
+    },
 };
-tamagochi.checkLife = function() {
-  if ((this.health) <= 0
+tamagochi.checkLife = function () {
+    if ((this.health) <= 0
     || (this.happiness) <= 0
     || (this.bellyful) <= 0) {
-      return false;
-
+        return false;
     }
     else {
-      return true
+      return true;
     }
-}  
+}; 
 tamagochi.showMassage = function () {
-  if (this.checkLife() === false) {
-    return 'happy pet'
-  } else {
-    return 'sorry, you pet died'
-  }
-}
+    if (this.checkLife() === false) {
+      return 'happy pet'
+    } else {
+      return 'sorry, you pet died'
+    }
+};
