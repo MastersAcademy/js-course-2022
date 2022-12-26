a1e14015228a3cf834654ba65a9cf8dc93c0c8be
Andrey Makarenko <baksckua@gmail.net>
2022-12-26 17:46:07 +0200

    homeworks-5

---
+++ homeworks/andrey.makarenko_Baksckua/5-arrays/arrays.js
@@ -0,0 +1,14 @@
+function height(tree) {
+    for (let i = 0; i < tree; i++) {
+        let snowflake = '*';
+        let empty = ' ';
+        const newYear = empty.repeat(tree - i - 1);
+        for (let a = 1; a <= i; a++) {
+            snowflake += '**';
+        }
+        snowflake = newYear + snowflake;
+        console.log(snowflake);
+    }
+}
+
+height(10);
