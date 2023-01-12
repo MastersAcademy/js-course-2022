471dbeade82bcb8eaaa3439c44929f5ca366b4c6
volkorezova <tanya.volkorezova@gmail.com>
2023-01-12 21:01:33 +0200

    Added HT#6 implementation

---
+++ homeworks/tatyana.volkorezova_volkorezova/6-async/async.js
@@ -0,0 +1,81 @@
+const tamagochi = {
+    name: 'Kiki',
+    healthValue: 5,
+    happinessValue: 5,
+    satietyValue: 3,
+    getLevelOfIndications() {
+        console.log(`The level of indications:
+            - healthValue: ${this.healthValue},\n
+            - happinessValue: ${this.happinessValue},\n
+            - satietyValue: ${this.satietyValue}\n`);
+    },
+    stateAnalyzer() {
+        const isAlive = this.healthValue >= 1
+            && this.happinessValue >= 1
+            && this.satietyValue >= 1;
+        if (isAlive) {
+            console.log(`Keep on. ${this.name} is still alive.`);
+        } else {
+            console.log(`Unfortunately, ${this.name} died. Game over.`);
+        }
+        return isAlive;
+    },
+    actions: {
+        healthUp() {
+            tamagochi.healthValue += 3;
+            tamagochi.happinessValue--;
+            tamagochi.satietyValue--;
+            console.log('Function name - healthUp()');
+            tamagochi.getLevelOfIndications();
+        },
+        happyUp() {
+            tamagochi.happinessValue++;
+            console.log('Function name - happyUp()');
+            tamagochi.getLevelOfIndications();
+        },
+        play() {
+            tamagochi.satietyValue--;
+            tamagochi.happinessValue--;
+            tamagochi.healthValue -= 3;
+            console.log('Function name - play()');
+            tamagochi.getLevelOfIndications();
+        },
+        feedUp() {
+            tamagochi.satietyValue++;
+            console.log('Function name - feedUp()');
+            tamagochi.getLevelOfIndications();
+        },
+        clean() {
+            tamagochi.healthValue++;
+            console.log('Function name - clean()');
+            tamagochi.getLevelOfIndications();
+        },
+        shopping() {
+            tamagochi.happinessValue++;
+            console.log('Function name - shopping()');
+            tamagochi.getLevelOfIndications();
+        },
+    },
+};
+const SEPARATOR = '*********************************';
+let tamagochiTimeOfLive = 0;
+let aliveStatus;
+
+function getRandonInt(value) {
+    return Math.floor(Math.random() * Math.floor(value));
+}
+const methods = Object.keys(tamagochi.actions);
+const methodsCount = methods.length;
+const call = (index) => tamagochi.actions[methods[index]]();
+
+const randFunc = setInterval(() => {
+    console.log(SEPARATOR);
+    call(getRandonInt(methodsCount));
+    aliveStatus = tamagochi.stateAnalyzer();
+    if (!aliveStatus) {
+        clearInterval(randFunc);
+    }
+    tamagochiTimeOfLive += 0.5;
+    console.log(`Time during tamagochi ${tamagochi.name} was alive: ${tamagochiTimeOfLive} seconds`);
+    console.log(SEPARATOR);
+}, 500);
