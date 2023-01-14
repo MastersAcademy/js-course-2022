74c631d1867d1b11c7fcaed2a81a7c447e636f27
Igor Zapisochnyy <109870514+IgorFromUA@users.noreply.github.com>
2023-01-14 10:31:29 +0200

    test version, not working

---
+++ homeworks/ihor.zapisochnyy_IgorFromUA/6-async/async.js
@@ -0,0 +1,57 @@
+const tamagochi = {
+    health: 5, // здоров'я
+    energy: 5, // єнергія
+    mood: 5, // настрій
+    eat() { // поїсти
+        this.health -= 2;
+        this.energy += 1;
+        this.mood += 1;
+        if (this.energy > 8) {
+            console.log('ааа... ЖИВІТ БОЛИТЬ ааа...');
+        } else {
+            console.log(`ням-ням смакота
+            eat
+            єнергія = ${this.energy} здоров'я = ${this.health} настрій = ${this.mood}`);
+        }
+    },
+    play() { // погратися
+        this.energy -= 2;
+        this.health += 1;
+        this.mood += 1;
+        if (this.energy < 1) {
+            console.log('я втомлений мені погано');
+        } else {
+            console.log(`ВАУ весело!!! Давай ще...
+            play
+            єнергія = ${this.energy} здоров'я = ${this.health} настрій = ${this.mood}`);
+        }
+    },
+    sleep() { // спати
+        this.mood -= 2;
+        this.health += 1;
+        this.energy += 1;
+        if (this.mood < 1) {
+            console.log('помер з нудьги');
+        } else {
+            console.log(`надобраніч
+            sleep 
+            єнергія = ${this.energy} здоров'я = ${this.health} настрій = ${this.mood}`);
+        }
+    },
+};
+
+const cat = Object.create(tamagochi, {
+    name: { value: 'Murka' },
+    legs: { value: 4 },
+    toPet: { value: () => console.log('Moorrr moorrrrr') },
+});
+
+const allMethods = ['eat', 'play', 'sleep'];
+
+cat.launchingRandomMethod = function (nintervalID) {
+    cat[allMethods[parseInt(Math.random() * 3, 10)]]();
+
+    if (cat.energy > 8 || cat.energy < 1 || cat.mood < 1) { clearInterval(nintervalID); }
+};
+
+// const intervalID = setInterval(cat.launchingRandomMethod(intervalID), 500);
