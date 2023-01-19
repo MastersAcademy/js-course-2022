f1f73947383464c8bca3e3eff9b27063b48dc125
YanSav10 <savinovyan@gmail.com>
2023-01-19 20:34:28 +0200

    Fix mistakes in class.js

--- homeworks/yan.savinov_YanSav10/7-class/class.js
+++ homeworks/yan.savinov_YanSav10/7-class/class.js
@@ -1,38 +1,27 @@
 class Car {
-    constructor() {
+    constructor(engine, wheels, type, size, weight, engineVolume, enginePower,
+        wheelsRadius, wheelsWidth, sizeHeight, sizeWidth, sizeLength) {
         this.car = {
-            engine: '',
-            wheels: '',
-            type: '',
-            size: '',
-            weight: '',
+            engine,
+            wheels,
+            type,
+            size,
+            weight,
         };
         this.engine = {
-            volume: '',
-            power: '',
+            volume: engineVolume,
+            power: enginePower,
         };
         this.wheels = {
-            radius: '',
-            width: '',
+            radius: wheelsRadius,
+            width: wheelsWidth,
         };
         this.size = {
-            height: '',
-            width: '',
-            length: '',
+            height: sizeHeight,
+            width: sizeWidth,
+            length: sizeLength,
         };
     }
 }
-const myCar = new Car();
-myCar.car = {
-    engine: 'gasoline', wheels: '4 wheels', type: 'Sedan', size: 'Mid-size car', weight: '2000 kg',
-};
-myCar.engine = {
-    volume: '4.0L', power: '450hp',
-};
-myCar.wheels = {
-    radius: '18inch', width: '8inch',
-};
-myCar.size = {
-    height: '180cm', width: '200cm', length: '300cm',
-};
+const myCar = new Car('gasoline', '4 wheels', 'Sedan', 'Mid-size car', '2000 kg', '4.0L', '450hp', '18inch', '8inch', '180cm', '200cm', '300cm');
 console.log(myCar);
