195c03f96db48925e98226d1a6d038ff7c09c0ea
YanSav10 <savinovyan@gmail.com>
2023-01-20 19:19:35 +0200

    Added async function in additional-task.js

--- homeworks/yan.savinov_YanSav10/7-class/additional-task.js
+++ homeworks/yan.savinov_YanSav10/7-class/additional-task.js
@@ -1,18 +1,39 @@
-const https = require('https');
+const http = require('http');
 
-class Request {
+class HttpClient {
     constructor(url) {
         this.url = url;
     }
 
-    makeRequest() {
-        https.get(this.url, (res) => {
-            res.on('data', (data) => {
-                console.log(JSON.parse(data));
+    async makeRequest() {
+        return new Promise((resolve, reject) => {
+            http.get(this.url, (res) => {
+                let data = '';
+                res.on('data', (chunk) => {
+                    data += chunk;
+                });
+                res.on('end', () => {
+                    try {
+                        resolve(JSON.parse(data));
+                    } catch (error) {
+                        reject(error);
+                    }
+                });
             });
         });
     }
 }
 
-const fetch = new Request(process.argv[2]);
-fetch.makeRequest();
+const run = async () => {
+    const url = process.argv[2];
+
+    const fetch = new HttpClient(url);
+    try {
+        const data = await fetch.makeRequest();
+        console.log(data);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+run();
