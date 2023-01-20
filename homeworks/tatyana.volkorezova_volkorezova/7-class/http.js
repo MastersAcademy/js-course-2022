e0caaabef480812959f59eacbfbae0a51432bd71
volkorezova <tanya.volkorezova@gmail.com>
2023-01-20 15:18:32 +0200

    Updated get request and added post

--- homeworks/tatyana.volkorezova_volkorezova/7-class/http.js
+++ homeworks/tatyana.volkorezova_volkorezova/7-class/http.js
@@ -1,32 +1,58 @@
-const http = require('http');
+const https = require('https');
 
 class GoToSite {
-    constructor(rout) {
-        this.rout = rout;
-    }
-
-    convertRoutString() {
-        return this.rout.slice(7);
+    constructor(url) {
+        this.url = url;
     }
 
     getRequest() {
-        return http.request({
-            hostname: this.convertRoutString(),
-        },
-        (res) => {
-            let data = '';
-            res.on('data', (d) => {
-                data += d;
+        return https
+            .get(`${this.url}/todos`, (res) => {
+                let data = '';
+                res.on('data', (d) => {
+                    data += d;
+                });
+                res.on('end', () => {
+                    console.log(JSON.parse(data));
+                });
+            })
+            .on('error', (err) => console.log('Error', err.message))
+            .end();
+    }
+
+    postRequest() {
+        const options = {
+            method: 'POST',
+            body: JSON.stringify({
+                title: 'foo',
+                body: 'bar',
+                userId: 1,
+            }),
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8',
+            },
+            timeout: 1000,
+        };
+        return new Promise((reject) => {
+            const req = https.request(`${this.url}/posts`, options, (res) => {
+                if (res.statusCode < 200 || res.statusCode > 299) {
+                    return reject(new Error(`HTTP status code ${res.statusCode}`));
+                }
+
+                return res.on('data', (chunk) => process.stdout.write(chunk));
             });
-            res.on('end', () => {
-                console.log(data);
+            req.on('error', (err) => {
+                console.log(err);
             });
-        }).on('error', console.error).end();
+            req.write(options.body);
+            req.end();
+        });
     }
 }
 if (process.argv.length === 2) {
-    console.log('Expected argument with url. Try again. I.E. node script.js \'https://www.google.com\'');
+    console.log('Error in call. I.E. node script.js \'https://jsonplaceholder.typicode.com\'');
 } else {
-    const goToGoogle = new GoToSite(process.argv[2]);
-    console.log(goToGoogle.getRequest());
+    const goToSite = new GoToSite(process.argv[2]);
+    goToSite.postRequest();
+    console.log(goToSite.getRequest());
 }
