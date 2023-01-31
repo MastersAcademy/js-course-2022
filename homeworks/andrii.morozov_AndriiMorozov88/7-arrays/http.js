1bc0186bee5fa3200c707c7571bd39a76a426625
Andrii Morozov <slon88888@ukr.net>
2023-01-31 15:27:15 +0200

    added methods

--- homeworks/andrii.morozov_AndriiMorozov88/7-arrays/http.js
+++ homeworks/andrii.morozov_AndriiMorozov88/7-arrays/http.js
@@ -1,6 +1,8 @@
 // const myUrl = 'https://jsonplaceholder.typicode.com/posts';
 const https = require('https');
 
+let body = '';
+
 const myUrl = process.argv[2];
 class HTTP {
     constructor(url) {
@@ -8,13 +10,21 @@ class HTTP {
     }
 
     async getData() {
-        https.get(this.url, (res) => {
-            let body = '';
-            res.on('data', (data) => {
-                body += data;
-            });
-            res.on('end', () => console.log(body));
-        });
+        https.get(this.url, (res) => this.fetchContent(res));
+    }
+
+    fetchContent(res) {
+        res.on('data', (data) => this.createContent(data));
+        res.on('end', () => this.showContent(body));
+    }
+
+    createContent(data) {
+        body += data;
+        return body;
+    }
+
+    showContent(content) {
+        console.log(content);
     }
 }
 const json = new HTTP(myUrl);
